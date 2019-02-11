66c5cbcc167d927c98c7c87a12ba21c3d8b38ab3
Jatheesh Parambath <Jatheesh.Parambath@anz.com>
2019-02-11 14:41:56 +0530

    Added test cases for Fib series checking

--- 01_fib/main.go
+++ 01_fib/main.go
@@ -2,20 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"strconv"
 )
 
+var out io.Writer = os.Stdout
+
 func main() {
-	fmt.Println(" Fibnacci Series")
+	//fmt.Fprintln(out, " Fibnacci Series")
 	fib(7)
 }
 
 func fib(limit int) {
 	firstNo := 0
 	secondNo := firstNo + 1
+	var finalString string
 	for i := 0; i < limit; i++ {
-		fmt.Println(firstNo)
+		//fmt.Println(firstNo)
+		finalString += strconv.Itoa(firstNo)
 		temp := secondNo
 		secondNo = secondNo + firstNo
 		firstNo = temp
 	}
+	//fmt.Println(finalString)
+	fmt.Fprintln(out, finalString)
 }
