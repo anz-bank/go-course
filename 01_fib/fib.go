f8b47925992e7d404bd8d87715f01a59861aa295
Satish Chandrasekaran <kumars59@anz.com>
2019-02-11 15:00:05 +0530

    Fibonacci series program

---
+++ 01_fib/fib.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func fib(n int) int {
+	f := make([]int, n+1, n+2)
+	if n < 2 {
+		f = f[0:2]
+	}
+	f[0] = 0
+	f[1] = 1
+	for i := 2; i <= n; i++ {
+		f[i] = f[i-1] + f[i-2]
+	}
+	return f[n]
+}
+
+func main() {
+	fmt.Println(fib(7))
+}
