1afb0ad8dba55f09ade961baac3e1b147d54023a
Yarragunta Mohanakrishna <Mohanakrishna.Yarragunta@anz.com>
2019-02-11 15:25:40 +0530

    Address PR comments
    Restructure the fibonacci computation logic

--- 01_fib/main.go
+++ 01_fib/main.go
@@ -14,20 +14,16 @@ func main() {
 
 func fib(n int) {
 
-	n1 := 0
-	n2 := 1
+	n1, n2 := 1, 1
 
-	fibSeries := ""
+	fibSeries := "0"
 
-	for i := 0; i < n; i++ {
-		if n1 == 0 {
-			fibSeries = fmt.Sprintf("%s%d", fibSeries, n1)
-		} else {
-			fibSeries = fmt.Sprintf("%d %s %d", n1*-1, fibSeries, n1)
-		}
+	for i := 1; i < n; i++ {
+
+		fibSeries = fmt.Sprintf("%d %s %d", -n1, fibSeries, n1)
+
 		sum := n1 + n2
-		n1 = n2
-		n2 = sum
+		n1, n2 = n2, sum
 	}
 
 	fmt.Fprint(out, fibSeries)
