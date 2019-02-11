560df3f13e30c06b70cb8dae742986176060e5f9
Satish Chandrasekaran <kumars59@anz.com>
2019-02-11 15:51:26 +0530

    Updated fib.go
    addressed review comments

--- 01_fib/fib.go
+++ 01_fib/fib.go
@@ -4,17 +4,16 @@ import (
 	"fmt"
 )
 
-func fib(n int) int {
-	f := make([]int, n+1, n+2)
+func fib(n int) []int {
+	f := make([]int, n)
 	if n < 2 {
 		f = f[0:2]
 	}
-	f[0] = 0
-	f[1] = 1
-	for i := 2; i <= n; i++ {
+	f[0], f[1] = 1, 1
+	for i := 2; i <= n-1; i++ {
 		f[i] = f[i-1] + f[i-2]
 	}
-	return f[n]
+	return f
 }
 
 func main() {
