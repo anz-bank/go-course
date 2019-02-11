346bfb2d1341ac54c40943d3ba1ea03eafa21454
Parag Vibhute <parag.vibhute@anz.com>
2019-02-11 12:41:37 +0530

    Printing fib series

--- 00_hello_world/main.go
+++ 00_hello_world/main.go
@@ -8,6 +8,21 @@ import (
 
 var out io.Writer = os.Stdout
 
+func fib(i int) int {
+	var a = 0
+	var b = 1
+	fmt.Println(b)
+	var sum int
+	for cnt := 0; cnt < i-1; cnt++ {
+		sum = a + b
+		fmt.Println(sum)
+		a = b
+		b = sum
+	}
+	return sum
+}
+
 func main() {
-	fmt.Fprintln(out, "Hallo du schöne Welt!")
+	fmt.Fprintln(out, "Hallo du schöne Welt2!")
+	fib(7)
 }
