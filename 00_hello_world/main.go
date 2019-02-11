23ea889bab053b29587cdaa813bb14863156028d
Akhtar <akhtars3@anz.com>
2019-02-11 13:51:58 +0530

    Created method to print Fibonacci series.

--- 00_hello_world/main.go
+++ 00_hello_world/main.go
@@ -9,5 +9,17 @@ import (
 var out io.Writer = os.Stdout
 
 func main() {
-	fmt.Fprintln(out, "Hallo du schöne Welt!")
+	fib(7)
+}
+
+func fib(n int){
+	f1 := 0
+	f2 := 1
+	fmt.Printf("%d\n",f2)
+	for counter := 1; counter < n; counter++ {
+		next := f1 + f2
+		fmt.Printf("%d\n",next)
+		f1 = f2
+		f2 = next
+	}
 }
