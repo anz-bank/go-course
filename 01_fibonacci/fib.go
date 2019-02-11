7416efafe72086820952ad8cad58bfc17b41ea28
Madhavan Subramanian <subrama8@anz.com>
2019-02-11 15:19:15 +0530

    Format the code

--- 01_fibonacci/fib.go
+++ 01_fibonacci/fib.go
@@ -9,22 +9,11 @@ import (
 
 var out io.Writer = os.Stdout
 
-// var (
-// 	sum       int
-// 	prev      int    = 0
-// 	cur       int    = 1
-// 	fibSeries string = ""
-// )
-
 func main() {
 	fibonacci(10)
 }
 
 func fibonacci(n int) {
-	// var sum int
-	// var prev int = 0
-	// var cur int = 1
-	// var fibSeries string = ""
 
 	var sum int
 	var prev, cur int = 0, 1
