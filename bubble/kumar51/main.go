2e0596e3f646220f9a7109fa53e5e16bb556505f
Kumar Pradip <kumar97@anz.com>
2019-02-12 14:38:18 +0530

    bubble sorting implemented

---
+++ bubble/kumar51/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var out io.Writer = os.Stdout
+
+func main() {
+	fmt.Fprint(out, bubble([]int{3, 2, 1, 5}))
+}
+
+func bubble(s []int) []int {
+
+	end := len(s) - 1
+	for {
+		if end == 0 {
+			break
+		}
+
+		for i := 0; i < len(s)-1; i++ {
+			if s[i] > s[i+1] {
+				s[i], s[i+1] = s[i+1], s[i]
+			}
+		}
+		end--
+	}
+	return s
+}
