a67fe0b58e44c17231747951816dcf22ffa7eef4
eobjects <eobjekts@gmail.com>
2019-02-12 13:06:01 +0530

    Calculate and print first n Fibonacci numbers and related tests

--- 01_fib/main.go
+++ 01_fib/main.go
@@ -4,18 +4,35 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
-var out io.Writer = os.Stdout
+var outWriter io.Writer = os.Stdout
 
-func fib(n int) {
-	a, b := 1, 1
-	for i := 0; i < n; i++ {
-		fmt.Fprintln(out, a)
-		a, b = b, a+b
+func calculateFib(n int) []int {
+	f0, f1, result := 1, 1, []int{}
+
+	for i := 1; i <= n; i++ {
+		result = append(result, f0)
+		f0, f1 = f1, f0+f1
 	}
+	return result
+}
+
+func fibo(n int) {
+	//ideally this can be printed in main but lab needs to be printed inside the func
+	printResult(calculateFib(n))
+}
+
+func printResult(arrayToPrint []int) {
+	resultsAsStr := []string{}
+	for i := 0; i < len(arrayToPrint); i++ {
+		resultsAsStr = append(resultsAsStr, strconv.Itoa(arrayToPrint[i]))
+	}
+	fmt.Fprintln(outWriter, strings.Join(resultsAsStr, "\n"))
 }
 
 func main() {
-	fib(7)
+	fibo(7)
 }
