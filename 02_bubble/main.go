96d4af4be8d8ce164b7104fdee8f5a57ffbc8378
Parag Vibhute <parag.vibhute@anz.com>
2019-02-12 12:06:54 +0530

    Sort array using bubble sorting logic

---
+++ 02_bubble/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var out io.Writer = os.Stdout
+
+func bubble(s []int) []int {
+	cnt := len(s)
+	for i := 0; i < cnt; i++ {
+		for j := i + 1; j < cnt; j++ {
+			if s[i] > s[j] {
+				tmp := s[i]
+				s[i] = s[j]
+				s[j] = tmp
+				j--
+			}
+		}
+	}
+	return s
+}
+
+func main() {
+	fmt.Println(bubble([]int{3, 2, 1, 5}))
+}
---
+++ 02_bubble/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBubbleOutput(t *testing.T) {
+	// Given
+	r := require.New(t)
+	var buf bytes.Buffer
+	out = &buf
+
+	// When
+	s := bubble([]int{3, 2, 1, 5})
+
+	// Then
+	expected := []int([]int{1, 2, 3, 5})
+	r.Equalf(expected, s, "Unexpected output in main()")
+}
