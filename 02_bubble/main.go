4955fdcf9b5b3bc980fd35c7271a061be9f8e42f
Yarragunta Mohanakrishna <Mohanakrishna.Yarragunta@anz.com>
2019-02-12 12:05:10 +0530

    Implement a method that sorts an integer slice using BubbleSort
    Add 02_bubble/main.go for implementing BubbleSort to an integer slice
    Add 02_bubble/main_test.go which contains unit tests to verify BubbleSort implementation

---
+++ 02_bubble/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var out io.Writer = os.Stdout
+
+func main() {
+	fmt.Fprint(out, bubble([]int{3, 2, 1, 5}))
+}
+
+func bubble(s []int) []int {
+	swapped := true
+	for swapped {
+		swapped = false
+		for i := 0; i < len(s)-1; i++ {
+			if s[i+1] < s[i] {
+				swapped = true
+				s[i+1], s[i] = s[i], s[i+1]
+			}
+		}
+	}
+	return s
+}
---
+++ 02_bubble/main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBubbleOutput(t *testing.T) {
+	// Given
+	r := require.New(t)
+	var buf bytes.Buffer
+	out = &buf
+
+	// When
+	main()
+
+	// Then
+	expected := "[1 2 3 5]"
+	actual := buf.String()
+	r.Equalf(expected, actual, "Unexpected output in main()")
+}
+
+var tests = []struct {
+	in  []int
+	out []int
+}{
+	{[]int{3, 2, 1, 5, -1, -90, -123, 4387, 29384}, []int{-123, -90, -1, 1, 2, 3, 5, 4387, 29384}},
+	{[]int{8, 5, 3, 1, 9, 6, 0, 7, 4, 2, 5}, []int{0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9}},
+	{[]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}}}
+
+func TestBubbleSortWithValaues(t *testing.T) {
+	for _, tt := range tests {
+		out := bubble(tt.in)
+		if !reflect.DeepEqual(out, tt.out) {
+			t.Errorf("got %v, want %v", out, tt.out)
+		}
+	}
+}
