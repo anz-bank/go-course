277f087c1147056068b600568ff84ba0b5f77a98
Amit Krishna <krisha13@corptst.anz.com>
2019-02-12 09:40:11 +0530

    ### Changes proposed in this PR: * Implemented code for Negative and zero output * Implemented code for Testing the negative and Zero output

--- 00_hello_world/main.go
+++ 00_hello_world/main.go
@@ -3,19 +3,34 @@ package main
 import (
 	"fmt"
 	"io"
+	"math"
 	"os"
 )
 
 var out io.Writer = os.Stdout
 
 func main() {
-	fibonacciSeries(7)
+	fibonacciSeries(-5)
 }
 
 func fibonacciSeries(number int) {
-	var a, b = 0, 1
-	for count := 0; count < number; count++ {
-		fmt.Fprintln(out, a)
+	if number == 0 {
+		return
+	}
+	var a, b = 1, 1
+	for count := 0; count < abs(number); count++ {
+		if number > 0 {
+			fmt.Fprintln(out, a)
+		} else {
+			fmt.Fprintln(out, math.Pow(-1, float64(count))*float64(a))
+		}
 		a, b = b, a+b
 	}
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
--- 00_hello_world/main_test.go
+++ 00_hello_world/main_test.go
@@ -17,7 +17,37 @@ func TestMainOutput(t *testing.T) {
 	main()
 
 	// Then
-	expected := "0\n1\n1\n2\n3\n5\n8\n"
+	expected := "1\n1\n2\n3\n5\n8\n13\n"
+	actual := buf.String()
+	r.Equalf(expected, actual, "Unexpected output in main()")
+}
+
+func TestFibonacciNegativeOutput(t *testing.T) {
+	// Given
+	r := require.New(t)
+	var buf bytes.Buffer
+	out = &buf
+
+	// When
+	fibonacciSeries(-5)
+
+	// Then
+	expected := "1\n-1\n2\n-3\n5\n"
+	actual := buf.String()
+	r.Equalf(expected, actual, "Unexpected output in main()")
+}
+
+func TestFibonacciZeroOutput(t *testing.T) {
+	// Given
+	r := require.New(t)
+	var buf bytes.Buffer
+	out = &buf
+
+	// When
+	fibonacciSeries(0)
+
+	// Then
+	expected := ""
 	actual := buf.String()
 	r.Equalf(expected, actual, "Unexpected output in main()")
 }
