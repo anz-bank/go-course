4b341eb18719daea12fc1367b78fc30b4299f1c3
GLOBAL\telkarr1 <raghavendra.telkar@anz.com>
2019-02-12 12:21:47 +0530

    Lab2-BubbleSort

---
+++ 02_bubble/bubble.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var out io.Writer = os.Stdout
+
+func bubble(s []int) []int {
+    n := len(s)
+    swapped := true
+    for swapped {
+        swapped = false
+        for i := 1; i < n; i++ {
+            if s[i-1] > s[i] {
+                s[i], s[i-1] = s[i-1], s[i]
+                swapped = true
+            }
+        }
+    }
+    return s
+}
+
+func main() {
+	fmt.Fprint(out, bubble([]int{3, 2, 1, 5}))
+}
