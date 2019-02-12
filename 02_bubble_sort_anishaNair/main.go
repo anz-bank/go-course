b36fc60540c83f6338722f4f791de19e8da2a96f
Anisha Nair <bnaira3@anz.com>
2019-02-12 13:04:14 +0530

    user name addition to folder

---
+++ 02_bubble_sort_anishaNair/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var out io.Writer = os.Stdout
+
+func bubble(n []int) []int {
+	if len(n) == 0 {
+		return n
+	}
+	for i := 0; i < len(n); i++ {
+		for j := 0; j < ((len(n) - i) - 1); j++ {
+			if n[j] > n[j+1] {
+				n[j+1], n[j] = n[j], n[j+1]
+			}
+		}
+	}
+	return n
+}
+
+func main() {
+	fmt.Fprintln(out, bubble([]int{3, 2, 1, 15, 13, 14, 50, 10}))
+}
