0da5e8c71080307de6be0165e862bbf77db26653
Madhavan Subramanian <subrama8@anz.com>
2019-02-12 16:11:03 +0530

    Test the insertion sort

--- 02_bubble/madhavansu/main_test.go
+++ 02_bubble/madhavansu/main_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"reflect"
 	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -25,39 +24,48 @@ func TestMainOutput(t *testing.T) {
 	r.Equalf(expected, actual, "Unexpected output in main()")
 }
 
-func TestBubbleAndInsertion(t *testing.T) {
+var testSet = map[string]map[string][]int{
+	"testSet1": {
+		"input":  {3, 2, 1, -2, 10, 19, 0, 81, -29},
+		"output": {-29, -2, 0, 1, 2, 3, 10, 19, 81},
+	},
+	"testSet2": {
+		"input":  {3, 2, 1},
+		"output": {1, 2, 3},
+	},
+	"testSet3": {
+		"input":  {20, 40, 80, 100},
+		"output": {20, 40, 80, 100},
+	},
+	"testSet4": {
+		"input":  {-3, 20, 19210, 110},
+		"output": {-3, 20, 110, 19210},
+	},
+}
+
+func TestBubbleSort(t *testing.T) {
 	// Given
 	r := require.New(t)
-	var buf bytes.Buffer
-	out = &buf
 
-	testSet := map[string]map[string][]int{
-		"testSet1": {
-			"input":  {3, 2, 1, -2, 10, 19, 0, 81, -29},
-			"output": {-29, -2, 0, 1, 2, 3, 10, 19, 81},
-		},
-		"testSet2": {
-			"input":  {3, 2, 1},
-			"output": {1, 2, 3},
-		},
-		"testSet3": {
-			"input":  {20, 40, 80, 100},
-			"output": {20, 40, 80, 100},
-		},
-		"testSet4": {
-			"input":  {-3, 20, 19210, 110},
-			"output": {-3, 20, 110, 19210},
-		},
-	}
-
-	for k, test := range testSet {
-		var pass bool
-		if strings.HasPrefix(k, "testSet") {
-			res := bubble(test["input"])
-			if !reflect.DeepEqual(res, test["output"]) {
-				pass = false
-			}
+	var pass bool
+	for _, test := range testSet {
+		res := bubble(test["input"])
+		if !reflect.DeepEqual(res, test["output"]) {
+			pass = false
 		}
-		r.Equalf(pass, false, "Unexpected output in main()")
 	}
+	r.Equalf(pass, false, "Unexpected output in bubble()")
+}
+func TestInsertionSort(t *testing.T) {
+	// Given
+	r := require.New(t)
+
+	var pass bool
+	for _, test := range testSet {
+		res := insertion(test["input"])
+		if !reflect.DeepEqual(res, test["output"]) {
+			pass = false
+		}
+	}
+	r.Equalf(pass, false, "Unexpected output in insertion()")
 }
