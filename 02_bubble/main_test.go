747f4b64bf403a542ba6bb84ed982ac0386f93d8
Parag Vibhute <parag.vibhute@anz.com>
2019-02-12 12:12:58 +0530

    Remove unnecessary conversion

--- 02_bubble/main_test.go
+++ 02_bubble/main_test.go
@@ -17,7 +17,7 @@ func TestBubbleOutput(t *testing.T) {
 	s := bubble([]int{3, 2, 1, 5})
 
 	// Then
-	expected := []int([]int{1, 2, 3, 5})
+	expected := []int{1, 2, 3, 5}
 	r.Equalf(expected, s, "Unexpected output in main()")
 }
 
