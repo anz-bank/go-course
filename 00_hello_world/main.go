15e143736e404ce37beb4e04ad07f3320fc59e71
Amit Krishna <krisha13@corptst.anz.com>
2019-02-12 08:19:04 +0530

    ### Changes proposed in this PR: * Implemented the changes as per the code review comment

--- 00_hello_world/main.go
+++ 00_hello_world/main.go
@@ -9,18 +9,13 @@ import (
 var out io.Writer = os.Stdout
 
 func main() {
-
 	fibonacciSeries(7)
 }
 
 func fibonacciSeries(number int) {
 	var a, b = 0, 1
-	fmt.Fprintln(out, a)
-	fmt.Fprintln(out, b)
-	for count := 0; count < number-2; count++ {
-		var sum = a + b
-		fmt.Fprintln(out, sum)
-		a, b = b, sum
-
+	for count := 0; count < number; count++ {
+		fmt.Fprintln(out, a)
+		a, b = b, a+b
 	}
 }
