121c80b36fb2c4c5c21e65c10f613564e00fdde5
GLOBAL\telkarr1 <raghavendra.telkar@anz.com>
2019-02-12 12:33:08 +0530

    `goimports`-ed fixes

--- 02_bubble/bubble.go
+++ 02_bubble/bubble.go
@@ -9,18 +9,18 @@ import (
 var out io.Writer = os.Stdout
 
 func bubble(s []int) []int {
-    n := len(s)
-    swapped := true
-    for swapped {
-        swapped = false
-        for i := 1; i < n; i++ {
-            if s[i-1] > s[i] {
-                s[i], s[i-1] = s[i-1], s[i]
-                swapped = true
-            }
-        }
-    }
-    return s
+	n := len(s)
+	swapped := true
+	for swapped {
+		swapped = false
+		for i := 1; i < n; i++ {
+			if s[i-1] > s[i] {
+				s[i], s[i-1] = s[i-1], s[i]
+				swapped = true
+			}
+		}
+	}
+	return s
 }
 
 func main() {
