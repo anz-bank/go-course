aec4c17e00fbc663c354d498e991ec9b1a36ff0f
Sandhya Mani <sandhya.mani@anz.com>
2019-02-12 14:28:11 +0530

    Remove temporary variable used for swapping

--- 02_bubble/02_bubble.go
+++ 02_bubble/02_bubble.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
+var out io.Writer = os.Stdout
+
 func main() {
-	fmt.Println(bubble([]int{3, 4, 1, 5, 2}))
+	fmt.Fprintln(out, bubble([]int{3, 4, 1, 5, 2}))
 }
 
 func bubble(s []int) []int {
-	var temp = 0
 	for i := 1; i < len(s); i++ {
 		for j := 0; j < len(s)-1; j++ {
 			if s[j] > s[j+1] {
-				temp = s[j]
-				s[j] = s[j+1]
-				s[j+1] = temp
+				s[j], s[j+1] = s[j+1], s[j]
 			}
 		}
 	}
--- 02_bubble/02_bubble_test.go
+++ 02_bubble/02_bubble_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -25,4 +27,18 @@ func TestBubbleSortOutputForNegativeValues(t *testing.T) {
 	actual := bubble([]int{20, 0, -10, 100, -5})
 	r.Equal(expected, actual)
 
+}
+func TestMainOutput(t *testing.T) {
+	// Given
+	r := require.New(t)
+	var buf bytes.Buffer
+	out = &buf
+
+	// When
+	main()
+
+	// Then
+	expected := strconv.Quote("[1 2 3 4 5]\n")
+	actual := strconv.Quote(buf.String())
+	r.Equalf(expected, actual, "Unexpected output in main()")
 }
