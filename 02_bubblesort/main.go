af0816dbcfa94b373e61dc1afb9cb867afd07cdd
ys1 <ys1@anz.com>
2019-02-12 12:35:23 +0530

    #Issue 42: LAB2 - bubble sort with unit tests

---
+++ 02_bubblesort/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var out io.Writer = os.Stdout
+
+func bubble(items []int) []int {
+	n := len(items)
+	sorted := false
+
+	for !sorted {
+		swapped := false
+		for i := 0; i < n-1; i++ {
+			if items[i] > items[i+1] {
+				items[i+1], items[i] = items[i], items[i+1]
+				swapped = true
+			}
+		}
+		if !swapped {
+			sorted = true
+		}
+		n = n - 1
+	}
+	return items
+}
+
+func main() {
+	fmt.Fprint(out, bubble([]int{3, 2, 1, 5}))
+}
