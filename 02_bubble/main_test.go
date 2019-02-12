dbcee07ce85563ce37a74e52115c6594e8071ee6
Parag Vibhute <parag.vibhute@anz.com>
2019-02-12 12:09:33 +0530

    Test case for sorting negative numbers

--- 02_bubble/main_test.go
+++ 02_bubble/main_test.go
@@ -20,3 +20,17 @@ func TestBubbleOutput(t *testing.T) {
 	expected := []int([]int{1, 2, 3, 5})
 	r.Equalf(expected, s, "Unexpected output in main()")
 }
+
+func TestBubbleOutputForNegativeNumbers(t *testing.T) {
+	// Given
+	r := require.New(t)
+	var buf bytes.Buffer
+	out = &buf
+
+	// When
+	s := bubble([]int{-3, -2, -1, -5})
+
+	// Then
+	expected := []int([]int{-5, -3, -2, -1})
+	r.Equalf(expected, s, "Unexpected output in main()")
+}
