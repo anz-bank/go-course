ba5b723c7894a89bf3856fcef47deb43c05dd70f
Madhavan Subramanian <subrama8@anz.com>
2019-02-12 09:54:28 +0530

    Format the output of Fibonacci series

--- 01_fibonacci/fib.go
+++ 01_fibonacci/fib.go
@@ -4,21 +4,18 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strconv"
 )
 
 var out io.Writer = os.Stdout
 
 func main() {
-	fmt.Fprint(out, fibonacci(10))
+	fibonacci(7)
 }
 
-func fibonacci(n int) string {
-	var fibSeries string
+func fibonacci(n int) {
 	a, b := 1, 1
 	for i := 1; i <= n; i++ {
-		fibSeries += strconv.Itoa(a)
+		fmt.Fprintln(out, a)
 		a, b = b, a+b
 	}
-	return fibSeries
 }
