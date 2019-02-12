91a612e7b9e4e5b9dd2d7c9de5cce7f1492b4a4e
Chitwan Agrawal <Chitwan.agrawal@Anz.com>
2019-02-12 10:24:07 +0530

    Address Review Comments

--- 01_fib/main.go
+++ 01_fib/main.go
@@ -8,17 +8,11 @@ import (
 
 var out io.Writer = os.Stdout
 
-var (
-	num1, num2 = 0, 1
-	sum        = 0
-)
-
 func fib(n int) {
-	fmt.Fprintln(out, num2)
-	for i := 1; i < n; i++ {
-		sum = num1 + num2
-		num1, num2 = num2, sum
-		fmt.Fprintln(out, sum)
+	var num1, num2 = 1, 1
+	for i := 0; i < n; i++ {
+		fmt.Fprintln(out, num1)
+		num1, num2 = num2, num1+num2
 	}
 }
 
