541ba2e45120046e22769df7f799b89fe9ede0fa
Satish Chandrasekaran <kumars59@anz.com>
2019-02-12 14:25:55 +0530

    Added unit test for main function

--- 02_bubble/satish/main_test.go
+++ 02_bubble/satish/main_test.go
@@ -7,6 +7,21 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestMainOutput(t *testing.T) {
+	// Given
+	r := require.New(t)
+	var buf bytes.Buffer
+	out = &buf
+
+	// When
+	main()
+
+	// Then
+	expected := `[1 2 3 5][1 2 3 5]`
+	actual := buf.String()
+	r.Equalf(expected, actual, "Unexpected output in main()")
+}
+
 func TestBubbleSortOutput(t *testing.T) {
 	// Given
 	r := require.New(t)
