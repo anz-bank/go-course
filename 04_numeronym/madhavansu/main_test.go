e2f9bcc95912512c017b3ffc7b69982f847bd0a4
Madhavan Subramanian <subrama8@anz.com>
2019-02-13 15:06:54 +0530

    Implement the tests for numeronym

--- 04_numeronym/madhavansu/main_test.go
+++ 04_numeronym/madhavansu/main_test.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+var s = []struct {
+	input  string
+	output string
+}{
+	{"accessibility", "[a11y]"},
+	{"Kubernetes", "[K8s]"},
+	{"World Wide Web", "[W12b]"},
+	{"abc", "[abc]"},
+}
 
 func TestMainOutput(t *testing.T) {
 	// Given
@@ -21,4 +32,16 @@ func TestMainOutput(t *testing.T) {
 	expected := strconv.Quote(`[a11y K8s abc]`)
 	actual := strconv.Quote(buf.String())
 	r.Equalf(expected, actual, "Unexpected output in main()")
+}
+
+func TestNumeronyms(t *testing.T) {
+	// Given
+	r := require.New(t)
+
+	// When
+	for _, v := range s {
+		expected := fmt.Sprint(numeronyms(v.input))
+		actual := v.output
+		r.Equalf(expected, actual, "Unexpected output in numeronyms()")
+	}
 }
--- 05_stringer/madhavansu/main_test.go
+++ 05_stringer/madhavansu/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"strconv"
 	"testing"
 
@@ -21,4 +22,25 @@ func TestMainOutput(t *testing.T) {
 	expected := strconv.Quote(`127.0.0.1`)
 	actual := strconv.Quote(buf.String())
 	r.Equalf(expected, actual, "Unexpected output in main()")
+}
+
+var s = []struct {
+	input  IPAddr
+	output string
+}{
+	{IPAddr{127, 0, 0, 1}, "127.0.0.1"},
+	{IPAddr{10, 10, 40, 1}, "10.10.40.1"},
+	{IPAddr{7, 20, 210, 12}, "7.20.210.12"},
+}
+
+func TestIPAddrInterface(t *testing.T) {
+	// Given
+	r := require.New(t)
+
+	// When
+	for _, v := range s {
+		expected := fmt.Sprint(v.input)
+		actual := v.output
+		r.Equalf(expected, actual, "Unexpected output in IPAddr()")
+	}
 }
