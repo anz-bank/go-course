d1c333520ed1fde7f3843d41175f673c1cc976fe
delvein <selvam_be2000@yahoo.co.in>
2019-02-13 15:21:09 +0530

    Update test with table driven test

--- 04_numeronym/selva/main_test.go
+++ 04_numeronym/selva/main_test.go
@@ -8,6 +8,21 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var nemeronymsTest = []struct {
+	in  []string
+	out []string
+}{
+	{[]string{"accessibility", "internationalisation"}, []string{"a11y", "i18n"}},
+}
+
+func TestMainTableDriven(t *testing.T) {
+	r := require.New(t)
+	for _, tt := range nemeronymsTest {
+		out := nemeronyms(tt.in...)
+		r.EqualValues(tt.out, out, "Unexpected output in main()")
+	}
+}
+
 func TestMainOutput(t *testing.T) {
 	// Given
 	r := require.New(t)
