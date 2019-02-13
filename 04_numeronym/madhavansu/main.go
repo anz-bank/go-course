be7d09c16b7d9f65dd72c27488bb1d4ced7c7e57
Madhavan Subramanian <subrama8@anz.com>
2019-02-13 19:26:25 +0530

    Change the logical implementation from string append to string pre-allocation

--- 04_numeronym/madhavansu/main.go
+++ 04_numeronym/madhavansu/main.go
@@ -13,14 +13,14 @@ func main() {
 	fmt.Fprint(out, numeronyms("accessibility", "Kubernetes", "abc"))
 }
 func numeronyms(vals ...string) []string {
-	var strcopy []string
-	for _, val := range vals {
+	strcopy := make([]string, len(vals))
+	for k, val := range vals {
 		val := []rune(strings.TrimSpace(val))
 		length := len(val)
 		if length > 3 {
-			strcopy = append(strcopy, fmt.Sprint(string(val[0:1]), length-2, string(val[length-1:])))
+			strcopy[k] = fmt.Sprint(string(val[0]), length-2, string(val[length-1]))
 		} else {
-			strcopy = append(strcopy, string(val))
+			strcopy[k] = string(val)
 		}
 	}
 	return strcopy
