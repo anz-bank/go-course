70ffbc78e5c86fea2e18274789e0a11009896d3b
Satish Chandrasekaran <kumars59@anz.com>
2019-02-13 15:50:01 +0530

    Make the IPAddr type implement fmt.Stringer to print the address as a dotted quad

---
+++ 05_stringer/satish/main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMainOutput(t *testing.T) {
+	// Given
+	r := require.New(t)
+	var buf bytes.Buffer
+	out = &buf
+
+	// When
+	main()
+
+	// Then
+	expected := "127.0.0.1\n"
+	actual := buf.String()
+	r.Equalf(expected, actual, "Unexpected output in main()")
+}
+
+// var tests = []struct {
+// 	in  []string
+// 	out []string
+// }{
+// 	{[]string{"internationalization", "Administration", "Africanization", "Appocalypse"}, []string{"i18n", "A12n",
+// 		"A12n", "A9e"}},
+// 	{[]string{}, []string{}},
+// }
+
+// func TestNumeronymsWithValues(t *testing.T) {
+// 	r := require.New(t)
+
+// 	for _, tt := range tests {
+// 		out := numeronyms(tt.in...)
+// 		r.EqualValues(tt.out, out)
+// 	}
+// }
