ffd928b799b92869e7fb2904eef40a28cbf643f3
eobjects <eobjekts@gmail.com>
2019-02-13 08:49:54 +0530

    Refactor to fix review comments

--- 03_letters/srini/main.go
+++ 03_letters/srini/main.go
@@ -12,13 +12,8 @@ var out io.Writer = os.Stdout
 
 func letters(s string) map[rune]int {
 	letterMap := make(map[rune]int)
-
 	for _, r := range s {
-		if _, ok := letterMap[r]; ok {
-			letterMap[r]++
-		} else {
-			letterMap[r] = 1
-		}
+		letterMap[r]++
 	}
 	return letterMap
 }
@@ -30,9 +25,7 @@ func sortLetters(m map[rune]int) []string {
 		keys = append(keys, int(k))
 	}
 	sort.Ints(keys)
-
 	sortedKeys := make([]string, 0, len(keys))
-
 	for _, k := range keys {
 		sortedKeys = append(sortedKeys, fmt.Sprintf("%s:%d", string(k), m[rune(k)]))
 	}
