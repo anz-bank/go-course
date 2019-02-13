2831b4e443ded8b627a7e4c78192dbe9b6b934da
Kumar Pradip <kumar97@anz.com>
2019-02-13 16:01:14 +0530

    Test case update

--- 04_numerym/kumar51/main_test.go
+++ 04_numerym/kumar51/main_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func numeroniumTest(t *testing.T) {
+func TestNumeroniumOutput(t *testing.T) {
 	// Given
 	r := require.New(t)
 	var buf bytes.Buffer
@@ -22,79 +22,4 @@ func numeroniumTest(t *testing.T) {
 	actual := buf.String()
 	r.Equalf(expected, actual, "Unexpected output in main()")
 }
-func testNoArgument(t *testing.T) {
-	// Given
-	r := require.New(t)
 
-	// When
-	m := numeronyms()
-
-	// Then
-	expected := 0
-	actual := len(m)
-	r.Equalf(expected, actual, "Unexpected output in main()")
-}
-func testNumeroniumsSingleArg(t *testing.T) {
-	// Given
-	r := require.New(t)
-
-	// When
-	a := numeronyms("abc")
-
-	// Then
-	expected := 1
-	actual := len(a)
-	r.Equalf(expected, actual, "Unexpected output in main()")
-}
-
-func emptyString(t *testing.T) {
-	// Given
-	r := require.New(t)
-
-	// When
-	m := prepareResponse("")
-
-	// Then
-	expected := ""
-	actual := m
-	r.Equalf(expected, actual, "Unexpected output in main()")
-}
-
-func lessThanThreeCharTesting(t *testing.T) {
-	// Given
-	r := require.New(t)
-
-	// When
-	m := prepareResponse("ab")
-
-	// Then
-	expected := "ab"
-	actual := m
-	r.Equalf(expected, actual, "Unexpected output in main()")
-}
-
-func threeCharTesting(t *testing.T) {
-	// Given
-	r := require.New(t)
-
-	// When
-	m := prepareResponse("abc")
-
-	// Then
-	expected := "abc"
-	actual := m
-	r.Equalf(expected, actual, "Unexpected output in main()")
-}
-
-func testGetNumeroniumsMultiCharString(t *testing.T) {
-	// Given
-	r := require.New(t)
-
-	// When
-	m := prepareResponse("Good morning")
-
-	// Then
-	expected := "G10g"
-	actual := m
-	r.Equalf(expected, actual, "Unexpected output in main()")
-}
