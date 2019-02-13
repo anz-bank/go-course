ee769c9650d09d15a96911eb1481c6b37ecb2208
Madhavan Subramanian <subrama8@anz.com>
2019-02-13 23:09:29 +0530

    Implement letter frequency logic and tests

--- 03_letters/madhavansu/main.go
+++ 03_letters/madhavansu/main.go
@@ -5,36 +5,32 @@ import (
 	"io"
 	"os"
 	"sort"
+	"strings"
 )
 
 var out io.Writer = os.Stdout
 
 func main() {
-	// fmt.Println(strings.Join(sortLetters(letters("aba")), "\n"))
-	// sortLetters(letters("aba"))
-	fmt.Println(sortLetters(letters("aba")))
+	fmt.Fprintln(out, strings.Join(sortLetters(letters("aba")), "\n"))
 }
 
 func letters(s string) map[rune]int {
-	letterMap := make(map[rune]int)
+	m := make(map[rune]int)
 	for _, v := range s {
-		letterMap[v]++
+		m[v]++
 	}
-	return letterMap
+	return m
 }
 
-func sortLetters(m map[rune]int) []rune {
-	// var sortedMap []string
-
-	sortedMap := make([]rune, 0, len(m))
-
-	for v := range m {
-		sortedMap = append(sortedMap, v)
+func sortLetters(m map[rune]int) []string {
+	keys := []int{}
+	for k := range m {
+		keys = append(keys, int(k))
 	}
-	sort.Ints(sortedMap)
-	for _, name := range sortedMap {
-		//...
+	sort.Ints(keys)
+	s := make([]string, len(keys))
+	for i, v := range keys {
+		s[i] = fmt.Sprintf("%c:%d", v, m[rune(v)])
 	}
-
-	return sortedMap
+	return s
 }
--- 03_letters/madhavansu/main_test.go
+++ 03_letters/madhavansu/main_test.go
@@ -8,6 +8,20 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var tests = []struct {
+	s    string
+	lmap map[rune]int
+	ss   []string
+}{
+
+	{"pwopoip@oiopoi@",
+		map[rune]int{'o': 5, 'p': 4, 'i': 3, '@': 2, 'w': 1},
+		[]string{"@:2", "i:3", "o:5", "p:4", "w:1"}},
+	{"‣£ê‣££~ 3 3 3",
+		map[rune]int{'3': 3, '£': 3, ' ': 3, '‣': 2, 'ê': 1, '~': 1},
+		[]string{" :3", "3:3", "~:1", "£:3", "ê:1", "‣:2"}},
+}
+
 func TestMainOutput(t *testing.T) {
 	// Given
 	r := require.New(t)
@@ -18,7 +32,18 @@ func TestMainOutput(t *testing.T) {
 	main()
 
 	// Then
-	expected := strconv.Quote(`[1 2 3 5]`)
+	expected := strconv.Quote(`a:2
+b:1
+`)
 	actual := strconv.Quote(buf.String())
 	r.Equalf(expected, actual, "Unexpected output in main()")
+}
+func TestLettersFrequency(t *testing.T) {
+	r := require.New(t)
+	for _, v := range tests {
+		expectedMap := letters(v.s)
+		r.Equal(v.lmap, expectedMap)
+		expectedString := sortLetters(v.lmap)
+		r.EqualValues(v.ss, expectedString)
+	}
 }
