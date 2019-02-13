7bd3b37f7599f6256333efce04e92cdbc47aecec
Parag Vibhute <parag.vibhute@anz.com>
2019-02-13 08:19:11 +0530

    Table driven tests

--- 02_bubble/paraganz/main_test.go
+++ 02_bubble/paraganz/main_test.go
@@ -7,6 +7,26 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var tests = []struct {
+	in  []int
+	out []int
+}{
+	{[]int{3, 2, 1, 5}, []int{1, 2, 3, 5}},
+	{[]int{-3, -2, -1, -5}, []int{-5, -3, -2, -1}},
+	{[]int{}, []int{}},
+	{[]int{5}, []int{5}},
+	{[]int{5, 3, 2, 1}, []int{1, 2, 3, 5}},
+	{[]int{5, 3, 5, 1}, []int{1, 3, 5, 5}},
+}
+
+func TestBubbleOutput(t *testing.T) {
+	r := require.New(t)
+	for _, tt := range tests {
+		out := bubble(tt.in)
+		r.ElementsMatch(tt.out, out)
+	}
+}
+
 func TestMainOutput(t *testing.T) {
 	// Given
 	r := require.New(t)
@@ -19,34 +39,6 @@ func TestMainOutput(t *testing.T) {
 	// Then
 	expected := "[1 2 3 5]\n"
 	r.Equalf(expected, buf.String(), "Unexpected output in main()")
-}
-
-func TestBubbleOutput(t *testing.T) {
-	// Given
-	r := require.New(t)
-	var buf bytes.Buffer
-	out = &buf
-
-	// When
-	s := bubble([]int{3, 2, 1, 5})
-
-	// Then
-	expected := []int{1, 2, 3, 5}
-	r.Equalf(expected, s, "Unexpected output in bubble()")
-}
-
-func TestBubbleOutputForNegativeNumbers(t *testing.T) {
-	// Given
-	r := require.New(t)
-	var buf bytes.Buffer
-	out = &buf
-
-	// When
-	s := bubble([]int{-3, -2, -1, -5})
-
-	// Then
-	expected := []int{-5, -3, -2, -1}
-	r.Equalf(expected, s, "Unexpected output in bubble()")
 }
 
 func TestBubbleDoesntModifyInput(t *testing.T) {
