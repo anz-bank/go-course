c5bdf3fb53296cbdf44a75e7221546e009fdede9
Akhtar <akhtars3@anz.com>
2019-02-14 18:17:01 +0530

    Fix review comment

--- 04_numeronym/sahidakhtar1/main.go
+++ 04_numeronym/sahidakhtar1/main.go
@@ -26,7 +26,7 @@ func createNumeronym(s string) string {
 	trimedString := strings.TrimSpace(s)
 	var numeronym string
 	//If the string contains space or \t or \n then it will return the same string
-	if strings.ContainsAny(trimedString, " & \t &\n") {
+	if strings.ContainsAny(trimedString, " \t\n") {
 		return s
 	}
 	runes := []rune(trimedString)
@@ -34,7 +34,7 @@ func createNumeronym(s string) string {
 	if count <= 3 {
 		numeronym = trimedString
 	} else {
-		numeronym = string(runes[0]) + strconv.Itoa(count-2) + string(runes[len(runes)-1])
+		numeronym = string(runes[0]) + strconv.Itoa(count-2) + string(runes[count-1])
 	}
 	return numeronym
 }
