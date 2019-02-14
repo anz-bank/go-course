7554b71c5961a31b2ba2d916665263cdb2ab6338
Jatheesh Parambath <Jatheesh.Parambath@anz.com>
2019-02-14 14:40:10 +0530

    Implement Lab6 Feature -Add Sorter interface with CRUD for Puppy Struct -Add implementations for map and Sync.Map as two Stores -Ass associated test cases.

---
+++ 06_puppy/Jat/puppyCRUD.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sync"
+)
+
+var out io.Writer = os.Stdout
+
+func main() {
+	pupStore := MapStore{}
+	pupStore.Initialize()
+	pup1 := pupStore.Create("Beagle", "brown", "Sam")
+	pup2 := pupStore.Read(pup1.Id)
+	pup2.Value = "Bingo"
+	pupStore.Update(pup2)
+	pupStore.Read(1)
+	pup4 := pupStore.Create("Bull", "brown", "Tyson")
+	pup5 := pupStore.Read(pup4.Id)
+	pupStore.Delete(pup1)
+	pupStore.Delete(pup5)
+	pupStore.Create("rottweiler", "black", "Tipu")
+	fmt.Fprintln(out, fmt.Sprintf("Items remaining in Store %d", len(pupStore.m)))
+}
+
+type Puppy struct {
+	Id     int
+	Breed  string
+	Colour string
+	Value  string
+}
+type Initializer interface {
+	Initialize()
+}
+type Storer interface {
+	Create(string, string, string) Puppy
+	Read(int) Puppy
+	Update(Puppy) int
+	Delete(Puppy) int
+}
+
+//Map Store
+type MapStore struct {
+	m map[int]Puppy
+}
+
+//Initializer Implementation for Mapstore
+func (store *MapStore) Initialize() {
+	store.m = make(map[int]Puppy)
+}
+
+//Storer  Implementation for Mapstore
+func (store *MapStore) Create(breed string, colour string, value string) Puppy {
+	id := len(store.m) + 1
+	newPup := Puppy{id, breed, colour, value}
+	store.m[id] = newPup
+	return newPup
+}
+func (store MapStore) Update(pup Puppy) int {
+	_, ok := store.m[pup.Id]
+	if !ok {
+		return -1
+	}
+	store.m[pup.Id] = pup
+	return 0
+}
+func (store MapStore) Delete(pup Puppy) int {
+	_, ok := store.m[pup.Id]
+	if !ok {
+		return -1
+	}
+	delete(store.m, pup.Id)
+	return 0
+}
+
+func (store MapStore) Read(id int) Puppy {
+	pup, _ := store.m[id]
+	return pup
+}
+
+//SuncMapstore
+type SyncMapStore struct {
+	m    sync.Map
+	sqid int
+}
+
+//Initializer Implementation for SyncMapstore
+func (store *SyncMapStore) Initialize() {
+	store.m = sync.Map{}
+}
+
+//Storer  Implementation for Mapstore
+func (store *SyncMapStore) Create(breed string, colour string, value string) Puppy {
+	store.sqid++
+	id := store.sqid
+	newPup := Puppy{id, breed, colour, value}
+	store.m.Store(id, newPup)
+	return newPup
+}
+func (store *SyncMapStore) Update(pup Puppy) int {
+	_, ok := store.m.Load(pup.Id)
+	if !ok {
+		return -1
+	}
+	store.m.Store(pup.Id, pup)
+	return 0
+}
+func (store *SyncMapStore) Delete(pup Puppy) int {
+	_, ok := store.m.Load(pup.Id)
+	if !ok {
+		return -1
+	}
+	store.m.Delete(pup.Id)
+	return 0
+}
+
+func (store *SyncMapStore) Read(id int) Puppy {
+	pup, _ := store.m.Load(id)
+	p, _ := pup.(Puppy)
+	return p
+}
---
+++ 06_puppy/Jat/puppyCRUD_test.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLettersMainOutput(t *testing.T) {
+	// Given
+	r := require.New(t)
+	var buf bytes.Buffer
+	out = &buf
+
+	// When
+	main()
+
+	// Then
+	expected := "Items remaining in Store 1\n"
+	actual := buf.String()
+	r.Equalf(expected, actual, "Unexpected output in main()")
+}
+func TestMapCreationSuccess(t *testing.T) {
+	r := require.New(t)
+	// When
+	pupStore := MapStore{}
+	pupStore.Initialize()
+	// Then
+	expected := 0
+	actual := len(pupStore.m)
+	r.EqualValues(expected, actual, "Unexpected output in TestMapCreation Success")
+}
+func TestCreatandReadPuppySuccess(t *testing.T) {
+	r := require.New(t)
+	// When
+	pupStore := MapStore{}
+	pupStore.Initialize()
+	npup := pupStore.Create("BullDog", "brown", "Tyson")
+	pup := pupStore.Read(npup.Id)
+	// Then
+	expected := "Tyson"
+	actual := pup.Value
+	r.EqualValues(expected, actual, "Unexpected output in TestMapCreation Success")
+}
+func TestCreatandReadandDeletePuppySuccess(t *testing.T) {
+	r := require.New(t)
+	// When
+	pupStore := MapStore{}
+	pupStore.Initialize()
+	npup := pupStore.Create("BullDog", "brown", "Tyson")
+	pupStore.Delete(npup)
+	// Then
+	expected := 0
+	actual := len(pupStore.m)
+	r.EqualValues(expected, actual, "Unexpected output in TestMapCreation Success")
+}
+func TestCreatandUpdateandReadPuppySuccess(t *testing.T) {
+	r := require.New(t)
+	// When
+	pupStore := MapStore{}
+	pupStore.Initialize()
+	npup := pupStore.Create("BullDog", "brown", "Tyson")
+	npup.Value = "Mike"
+	pupStore.Update(npup)
+	upupup := pupStore.Read(npup.Id)
+	// Then
+	expected := "Mike"
+	actual := upupup.Value
+	r.EqualValues(expected, actual, "Unexpected output in TestMapCreation Success")
+}
+
+func TestUpdateNonExisting(t *testing.T) {
+	r := require.New(t)
+	// When
+	pupStore := MapStore{}
+	pupStore1 := MapStore{}
+	pupStore.Initialize()
+	pupStore1.Initialize()
+	npup := pupStore.Create("BullDog", "brown", "Tyson")
+	npup.Value = "Mike"
+	res := pupStore1.Update(npup)
+	// Then
+	expected := -1
+	actual := res
+	r.EqualValues(expected, actual, "Unexpected output in TestMapCreation Success")
+}
+
+func TestDeleteNonExisting(t *testing.T) {
+	r := require.New(t)
+	// When
+	pupStore := MapStore{}
+	pupStore1 := MapStore{}
+	pupStore.Initialize()
+	pupStore1.Initialize()
+	npup := pupStore.Create("BullDog", "brown", "Tyson")
+	npup.Value = "Mike"
+	res := pupStore1.Delete(npup)
+	// Then
+	expected := -1
+	actual := res
+	r.EqualValues(expected, actual, "Unexpected output in TestMapCreation Success")
+}
+
+// Test for SyncMap
+func TestCreatandReadPuppySyncSuccess(t *testing.T) {
+	r := require.New(t)
+	// When
+	pupStore := SyncMapStore{}
+	pupStore.Initialize()
+	npup := pupStore.Create("BullDog", "brown", "Tyson")
+	pup := pupStore.Read(npup.Id)
+	// Then
+	expected := "Tyson"
+	actual := pup.Value
+	r.EqualValues(expected, actual, "Unexpected output in TestMapCreation Success")
+}
+func TestCreatandReadandDeleteSyncPuppySuccess(t *testing.T) {
+	r := require.New(t)
+	// When
+	pupStore := SyncMapStore{}
+	pupStore.Initialize()
+	npup := pupStore.Create("BullDog", "brown", "Tyson")
+	pupStore.Delete(npup)
+	npup1 := pupStore.Read(npup.Id)
+	// Then
+	expected := npup1.Value
+	actual := ""
+	r.EqualValues(expected, actual, "Unexpected output in TestMapCreation Success")
+}
+func TestCreatandUpdateandReadSyncPuppySuccess(t *testing.T) {
+	r := require.New(t)
+	// When
+	pupStore := SyncMapStore{}
+	pupStore.Initialize()
+	npup := pupStore.Create("BullDog", "brown", "Tyson")
+	npup.Value = "Mike"
+	pupStore.Update(npup)
+	upupup := pupStore.Read(npup.Id)
+	// Then
+	expected := "Mike"
+	actual := upupup.Value
+	r.EqualValues(expected, actual, "Unexpected output in TestMapCreation Success")
+}
+
+func TestUpdateNonSyncExisting(t *testing.T) {
+	r := require.New(t)
+	// When
+	pupStore := SyncMapStore{}
+	pupStore1 := SyncMapStore{}
+	pupStore.Initialize()
+	pupStore1.Initialize()
+	npup := pupStore.Create("BullDog", "brown", "Tyson")
+	npup.Value = "Mike"
+	res := pupStore1.Update(npup)
+	// Then
+	expected := -1
+	actual := res
+	r.EqualValues(expected, actual, "Unexpected output in TestMapCreation Success")
+}
+
+func TestDeleteNonSyncExisting(t *testing.T) {
+	r := require.New(t)
+	// When
+	pupStore := SyncMapStore{}
+	pupStore1 := SyncMapStore{}
+	pupStore.Initialize()
+	pupStore1.Initialize()
+	npup := pupStore.Create("BullDog", "brown", "Tyson")
+	npup.Value = "Mike"
+	res := pupStore1.Delete(npup)
+	// Then
+	expected := -1
+	actual := res
+	r.EqualValues(expected, actual, "Unexpected output in TestMapCreation Success")
+}
