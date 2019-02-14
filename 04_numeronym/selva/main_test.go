ed8bb005721e22830540f61c3c50cdf650e1d06f
delvein <selvam_be2000@yahoo.co.in>
2019-02-14 11:17:16 +0530

    Fix test coverage

--- 04_numeronym/selva/main_test.go
+++ 04_numeronym/selva/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -21,4 +23,19 @@ func TestNumeronyms(t *testing.T) {
 		out := numeronyms(tt.in...)
 		r.EqualValues(tt.expectedOut, out, "Unexpected output in main()")
 	}
+}
+
+func TestMainOutput(t *testing.T) {
+	// Given
+	r := require.New(t)
+	var buf bytes.Buffer
+	out = &buf
+
+	// When
+	main()
+
+	// Then
+	expected := strconv.Quote("[a11y K8s abc a2d]")
+	actual := strconv.Quote(buf.String())
+	r.Equalf(expected, actual, "Unexpected output in main()")
 }
