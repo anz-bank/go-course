f18045fdecf2faf5818c1705d3e2fa198ba0ef28
Mohankrishna Yarragunta <mohankrishna@users.noreply.github.com>
2019-03-07 11:51:49 +0530

    Lab 8 & 9 - JSON puppy, LevelDB and Kingpin arg parsing (#118)
    * Create the project structure
    
    * Add a new storer implementation with Level DB
    
    * Add command line flag -d FILE with long-form --data FILE using kingpin.v2
    
    * Add Mutex locking for sync store  and LevelDB

---
+++ 08_project/mohankrishna/pkg/mohankrishna-puppy/store/sync_store.go
@@ -0,0 +1,60 @@
+package store
+
+import (
+	"fmt"
+	"sync"
+
+	types "github.com/anz-bank/go-training/08_project/mohankrishna/pkg/mohankrishna-puppy"
+)
+
+type SyncStore struct {
+	sm    sync.Map
+	mutex sync.Mutex
+}
+
+func NewSyncStore() *SyncStore {
+	return &SyncStore{}
+}
+
+func (ss *SyncStore) CreatePuppy(puppy *types.Puppy) error {
+	ss.mutex.Lock()
+	defer ss.mutex.Unlock()
+	if _, ok := ss.sm.Load(puppy.ID); ok {
+		return &types.Error{Code: types.ErrDuplicate,
+			Message: fmt.Sprintf("A puppy with ID: %d already exists", puppy.ID)}
+	}
+	ss.sm.Store(puppy.ID, puppy)
+	return nil
+}
+
+func (ss *SyncStore) ReadPuppy(id uint32) (*types.Puppy, error) {
+	v, ok := ss.sm.Load(id)
+	if ok {
+		puppy, ok := v.(*types.Puppy)
+		if ok {
+			return puppy, nil
+		}
+	}
+	return nil, &types.Error{Code: types.ErrNotFound,
+		Message: fmt.Sprintf("A puppy with ID: %d doesn't exist", id)}
+}
+
+func (ss *SyncStore) UpdatePuppy(id uint32, puppy *types.Puppy) error {
+	if id != puppy.ID {
+		return &types.Error{Code: types.ErrInvalidInput,
+			Message: fmt.Sprintf("The id:%d passed and the puppy's id:%d doesnt match", id, puppy.ID)}
+	}
+	ss.sm.Store(id, puppy)
+	return nil
+}
+
+func (ss *SyncStore) DeletePuppy(id uint32) error {
+	ss.mutex.Lock()
+	defer ss.mutex.Unlock()
+	_, ok := ss.sm.Load(id)
+	if ok {
+		ss.sm.Delete(id)
+		return nil
+	}
+	return &types.Error{Code: types.ErrNotFound, Message: fmt.Sprintf("No puppy exists with id %d", id)}
+}
