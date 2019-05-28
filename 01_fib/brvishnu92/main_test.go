95c799bcbf57de11943da25697fe54b0f8481bf1
Vishnu Shanker <vishnu.shanker@anz.com>
2019-05-28 10:58:09 +1000

    Lint code test coverage 100%

--- 01_fib/brvishnu92/main_test.go
+++ 01_fib/brvishnu92/main_test.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"strconv"
 	"testing"
-	"fmt"
 )
+func TestMain(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
 
-func TestMainOutput(t *testing.T) {
+	main()
+
+	expected := strconv.Quote("1\n1\n2\n3\n5\n8\n13\n")
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		fmt.Println(actual)
+		t.Errorf("Unexpected output in main()")
+	}
+
+}
+
+func TestMainOutputPositive(t *testing.T) {
 	var buf bytes.Buffer
 	out = &buf
 
@@ -21,6 +36,36 @@ func TestMainOutput(t *testing.T) {
 		t.Errorf("Unexpected output in main()")
 	}
 
-	
+}
+
+func TestMainOutputNegative(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	fib(-7)
+
+	expectedNeg := strconv.Quote("1\n-1\n2\n-3\n5\n-8\n13\n")
+	actualNeg := strconv.Quote(buf.String())
+
+	if expectedNeg != actualNeg {
+		fmt.Println(actualNeg)
+		t.Errorf("Unexpected output in main()")
+	}
+
+}
+
+func TestMainOutputZero(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	fib(0)
+
+	expectedNeg := strconv.Quote("0\n")
+	actualNeg := strconv.Quote(buf.String())
+
+	if expectedNeg != actualNeg {
+		fmt.Println(actualNeg)
+		t.Errorf("Unexpected output in main()")
+	}
 
 }
