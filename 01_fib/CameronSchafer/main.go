a20b6df8177f0204f18445beec970b25930523c6
Cameron Schafer <cameron.schafer@anz.com>
2019-05-28 11:28:15 +1000

    Add function for calculating the next number in the Fibonacci sequence

--- 01_fib/CameronSchafer/main.go
+++ 01_fib/CameronSchafer/main.go
@@ -10,7 +10,6 @@ var out io.Writer = os.Stdout
 
 //base fibonacci function
 func fib(n int){
-	//testing the normal fib sequence function with actual input.
 	sequence := calculateNormalFib(7)
 	printFibSequence(sequence)
 }
@@ -26,14 +25,21 @@ func calculateNormalFib(x int) []int{
 	//loop until end of sequence.
 	for count := 0; count < x; count++{
 		calcd_sequence = append(calcd_sequence, n2)	//store n2 into array
-		n1,n2 = n2, n1+n2	//calc next value in the sequence + assign n1 to the old value of n2.
+		n1,n2 = n2, calcNextInSequence(n1,n2)	//calc next value in the sequence + assign n1 to the old value of n2.
 	}
 
 	return calcd_sequence
 }
 
-func calculateNegaFib(x int){
-	
+func calculateNegaFib(x int) []int{
+	var calcd_sequence []int
+	return calcd_sequence
+}
+
+//function calculates the next number in the fibonacci sequence.
+func calcNextInSequence(n1 int, n2 int) int{
+	nextInSequence := n1 + n2
+	return nextInSequence
 }
 
 //loop through and print the sequence.
