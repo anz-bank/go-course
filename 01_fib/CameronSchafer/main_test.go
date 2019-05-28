3cb370de969b25b99daf322276badd80d1abb827
Cameron Schafer <cameron.schafer@anz.com>
2019-05-28 14:49:44 +1000

    Fix imported a module and never used it Add test case for fib(7)

--- 01_fib/CameronSchafer/main_test.go
+++ 01_fib/CameronSchafer/main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"reflect"
 	"strconv"
 	"testing"
 )
@@ -19,5 +18,20 @@ func TestMainOutput(t *testing.T) {
 
 	if expected != actual {
 		t.Errorf("Unexpected output in main()")
+	}
+}
+
+//FibOutput test
+func TestFibOutput(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	fib(7)
+
+	expected := strconv.Quote("1\n1\n2\n3\n5\n8\n13\n")
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		t.Errorf("Unexpected output in fib(7)")
 	}
 }
