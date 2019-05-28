5073943c640b7f4d155a7e299d403d042fcec852
Cameron Schafer <cameron.schafer@anz.com>
2019-05-28 14:50:36 +1000

    Add test case for Normal fib sequence finder function Add test case for Nega fib sequence finder function

--- 01_fib/CameronSchafer/main_test.go
+++ 01_fib/CameronSchafer/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"strconv"
 	"testing"
 )
@@ -33,5 +34,33 @@ func TestFibOutput(t *testing.T) {
 
 	if expected != actual {
 		t.Errorf("Unexpected output in fib(7)")
+	}
+}
+
+//NormalFibOutput test
+func TestNormalFibOutput(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	expected := []int{1, 1, 3, 5, 8, 13}
+	actual := calculateNormalFib(7)
+
+	if reflect.DeepEqual(actual, expected) {
+		t.Errorf("Unexpected output in calculateNormalFib(7)")
+	}
+}
+
+//NegaFibOutput test
+func TestNegaFibOutput(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	calculateNegaFib(7)
+
+	expected := []int{1, -1, 3, -5, 8, -13}
+	actual := calculateNegaFib(7)
+
+	if reflect.DeepEqual(actual, expected) {
+		t.Errorf("Unexpected output in calculateNegaFib(7)")
 	}
 }
