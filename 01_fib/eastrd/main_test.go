40c9b514806c088a2350994ed2a97e8c11301166
eastrd <yjia285@student.monash.edu>
2019-05-28 15:33:04 +1000

    Change main_test to read from buffer instead

--- 01_fib/eastrd/main_test.go
+++ 01_fib/eastrd/main_test.go
@@ -1,44 +1,84 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"strconv"
 	"testing"
 )
 
 func TestFib1(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
 
-	expected := 5
-	actual, err := fib(5)
+	fib(7)
 
-	if expected != actual && err != nil {
-		t.Errorf("Error in fib function")
+	expected := strconv.Quote("1\n1\n2\n3\n5\n8\n13\n")
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		t.Errorf("Unexpected output in main()")
 	}
 }
 
 func TestFib2(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
 
-	expected := 1
-	actual, err := fib(1)
+	fib(1)
 
-	if expected != actual && err != nil {
-		t.Errorf("Error in fib function")
+	expected := strconv.Quote("1\n")
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		t.Errorf("Unexpected output in main()")
 	}
 }
 
 func TestFib3(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
 
-	expected := 1
-	actual, err := fib(2)
+	fib(0)
 
-	if expected != actual && err != nil {
-		t.Errorf("Error in fib function")
+	expected := strconv.Quote("Fibonacci number has to be positive\n")
+	actual := strconv.Quote(buf.String())
+
+	fmt.Println(actual)
+
+	if expected != actual {
+		t.Errorf("Unexpected output in main()")
 	}
 }
 
 func TestFib4(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
 
-	_, err := fib(-1)
+	fib(4)
 
-	if err == nil {
-		t.Errorf("Error in fib function")
+	expected := strconv.Quote("1\n1\n2\n3\n")
+	actual := strconv.Quote(buf.String())
+
+	fmt.Println(actual)
+
+	if expected != actual {
+		t.Errorf("Unexpected output in main()")
+	}
+}
+
+func TestFib5(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	fib(2)
+
+	expected := strconv.Quote("1\n1\n")
+	actual := strconv.Quote(buf.String())
+
+	fmt.Println(actual)
+
+	if expected != actual {
+		t.Errorf("Unexpected output in main()")
 	}
 }
