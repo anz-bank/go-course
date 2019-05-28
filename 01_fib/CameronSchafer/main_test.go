ca1bbda4bd8da7652ec96a886c159261cfdc0171
Cameron Schafer <cameron.schafer@anz.com>
2019-05-28 14:48:39 +1000

    Add test case for main function

--- 01_fib/CameronSchafer/main_test.go
+++ 01_fib/CameronSchafer/main_test.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"strconv"
 	"testing"
 )
 
+//main test
 func TestMainOutput(t *testing.T) {
 	var buf bytes.Buffer
 	out = &buf
 
 	main()
 
-	expected := strconv.Quote("Hallo du schöne Welt!\n")
+	expected := strconv.Quote("1\n1\n2\n3\n5\n8\n13\n")
 	actual := strconv.Quote(buf.String())
 
 	if expected != actual {
-		//t.Errorf("Unexpected output in main()")
+		t.Errorf("Unexpected output in main()")
 	}
 }
