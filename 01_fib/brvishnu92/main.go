7e3cc2ff2048fdf8915ed9ead838370d68e8ffac
Vishnu Shanker <vishnu.shanker@anz.com>
2019-05-28 10:10:42 +1000

    Update Fib to include negafib

--- 01_fib/brvishnu92/main.go
+++ 01_fib/brvishnu92/main.go
@@ -10,17 +10,31 @@ import (
 var out io.Writer = os.Stdout
 
 func main() {
-	fib(-7)
+	fib(7)
 }
 
 
 func fib(n int){
+	if(n>0){
+		printFib(n,"positive")
+	}else if(n<0){
+		printFib(n,"negative")
+	}
+	}
+
+
+func printFib(n int,valtype string){
 
 	var first,second,count,sum int = 1, 0,1,0
 	n= int(math.Abs(float64(n)))
 	for count<=n {
 	sum=first+second
-	fmt.Fprintln(out,sum)
+	if(valtype=="negative" && count%2==0){
+		
+		fmt.Fprintln(out,sum*-1)	
+	}else{
+		fmt.Fprintln(out,sum)
+	}
 	first=second
 	second=sum
 	count+=1
