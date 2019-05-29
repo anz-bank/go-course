8607232132e071ad53e18ea63264648e71a44034
Li, Luliang <luliang.li@anz.com>
2019-05-29 16:00:00 +1000

    rename the function name

--- 01_fib/luliangLi/main.go
+++ 01_fib/luliangLi/main.go
@@ -9,7 +9,7 @@ const (
 	fibTwo     = 1
 )
 
-func fib_tail_nega(n int64, first int64, second int64) int64 {
+func fibTailNega(n int64, first int64, second int64) int64 {
 	fmt.Println(second)
 	switch n {
 	case 0:
@@ -17,11 +17,11 @@ func fib_tail_nega(n int64, first int64, second int64) int64 {
 	case -1 :
 		return fibNegaone
 	default :
-		return fib_tail_nega(n + 1, second, first - second )
+		return fibTailNega(n + 1, second, first - second )
 	}
 }
 
-func fib_tail(n int64, first int64, second int64) int64 {
+func fibTail(n int64, first int64, second int64) int64 {
 	fmt.Println(second)
 	switch n {
 	case 1 :
@@ -29,19 +29,19 @@ func fib_tail(n int64, first int64, second int64) int64 {
 	case 2 :
 		return second
 	default :
-		return fib_tail(n-1, second, first + second)
+		return fibTail(n-1, second, first + second)
 	}
 }
 
 func fib(n int) int64 {
 	if n <= 0 {
 		fmt.Println(fibZero)
-		return fib_tail_nega(int64(n), fibZero, fibNegaone)
+		return fibTailNega(int64(n), fibZero, fibNegaone)
 	}
 
 	if n > 0{
 		fmt.Println(fibOne)
-		return fib_tail(int64(n), fibOne, fibTwo)
+		return fibTail(int64(n), fibOne, fibTwo)
 	}
 
 	// add this to pass golint check...
