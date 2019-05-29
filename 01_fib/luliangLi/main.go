53e7efba573ea727cb77bb21061fe6d82b2a811a
Li, Luliang <luliang.li@anz.com>
2019-05-29 15:55:08 +1000

    bug fix

--- 01_fib/luliangLi/main.go
+++ 01_fib/luliangLi/main.go
@@ -3,45 +3,53 @@ package main
 import "fmt"
 
 const (
-	fib_NEGAONE = 1
-	fib_ZERO = 0
-	fib_ONE = 1
-	fib_TWO = 1
+	fibNegaone = 1
+	fibZero    = 0
+	fibOne     = 1
+	fibTwo     = 1
 )
 
 func fib_tail_nega(n int64, first int64, second int64) int64 {
 	fmt.Println(second)
-	if n == 0 {
-		return fib_ZERO
-	} else if n == -1 {
-		return fib_NEGAONE
-	} else {
+	switch n {
+	case 0:
+		return fibZero
+	case -1 :
+		return fibNegaone
+	default :
 		return fib_tail_nega(n + 1, second, first - second )
 	}
 }
 
 func fib_tail(n int64, first int64, second int64) int64 {
 	fmt.Println(second)
-	if n == 1 {
+	switch n {
+	case 1 :
 		return first
-	} else if n == 2 {
+	case 2 :
 		return second
-	} else {
+	default :
 		return fib_tail(n-1, second, first + second)
 	}
 }
 
 func fib(n int) int64 {
 	if n <= 0 {
-		fmt.Println(fib_ZERO)
-		return fib_tail_nega(int64(n), fib_ZERO, fib_NEGAONE)
-	} else {
-		fmt.Println(fib_ONE)
-		return fib_tail(int64(n), fib_ONE, fib_TWO)
+		fmt.Println(fibZero)
+		return fib_tail_nega(int64(n), fibZero, fibNegaone)
 	}
+
+	if n > 0{
+		fmt.Println(fibOne)
+		return fib_tail(int64(n), fibOne, fibTwo)
+	}
+
+	// add this to pass golint check...
+	return 0
 }
 
 func main()  {
 	fib(7)
+	fmt.Println("********************")
 	fib(-7)
 }
