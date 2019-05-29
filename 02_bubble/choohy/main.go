d3d28b3e56d58b3e55071e5d114a80e0f6739b20
Choo, Hwan-Yi <Hwan-Yi.Choo@anz.com>
2019-05-29 23:59:35 +1000

    Test corner cases

--- 02_bubble/choohy/main.go
+++ 02_bubble/choohy/main.go
@@ -13,26 +13,59 @@ func main() {
 
 	fmt.Fprintln(out, bubble([]int{3, 2, 1, 5}))
 
-	a, b := swapInt(2, 1)
-	fmt.Println(a, b)
+	// a, b := swapInt(2, 1)
+	// fmt.Println(a, b)
 }
 
 func bubble(s []int) []int {
 
-	//fmt.Println(len(s))
+	var min = minValue(s)
+	var max = maxValue(s)
+	//fmt.Println("min", min, "max", max, "s", s)
 
 	for i := 0; i < len(s); i++ {
 		next := i + 1
-		fmt.Println("i", i, "next", next, "s", s)
+		//fmt.Println("i", i, "next", next, "s", s)
 		if next < len(s) && s[i] > s[next] {
 			a, b := swapInt(s[i], s[next])
 			s[i] = a
 			s[next] = b
 		}
 	}
+	if s[0] != min || s[len(s)-1] != max {
+		//fmt.Println("ha ha not done yet")
+		bubble(s)
+	}
+
 	return s
 }
 
 func swapInt(x, y int) (int, int) {
 	return y, x
+}
+
+func minValue(s []int) (min int) {
+	min = 0
+	if len(s) > 0 {
+		min = s[0]
+	}
+	for i := 0; i < len(s); i++ {
+		if min > s[i] {
+			min = s[i]
+		}
+	}
+	return
+}
+
+func maxValue(s []int) (max int) {
+	max = 0
+	if len(s) > 0 {
+		max = s[0]
+	}
+	for i := 0; i < len(s); i++ {
+		if max < s[i] {
+			max = s[i]
+		}
+	}
+	return
 }
--- 02_bubble/choohy/main_test.go
+++ 02_bubble/choohy/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"strconv"
 	"testing"
 )
@@ -14,6 +15,86 @@ func TestBubble(t *testing.T) {
 
 	expected := strconv.Quote(`Bubble Sort!
 [1 2 3 5]
+`)
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		t.Errorf(expected)
+		t.Errorf(actual)
+	}
+}
+
+func TestBubble0(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	fmt.Fprintln(out, bubble([]int{0, 0, 0, 0}))
+
+	expected := strconv.Quote(`[0 0 0 0]
+`)
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		t.Errorf(expected)
+		t.Errorf(actual)
+	}
+}
+
+func TestBubbleNegative(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	fmt.Fprintln(out, bubble([]int{-1, -6, -3, -10}))
+
+	expected := strconv.Quote(`[-10 -6 -3 -1]
+`)
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		t.Errorf(expected)
+		t.Errorf(actual)
+	}
+}
+
+func TestBubblePositiveNegative(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	fmt.Fprintln(out, bubble([]int{-1, 6, 3, -10}))
+
+	expected := strconv.Quote(`[-10 -1 3 6]
+`)
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		t.Errorf(expected)
+		t.Errorf(actual)
+	}
+}
+
+func TestBubbleDuplicates(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	fmt.Fprintln(out, bubble([]int{6, 6, 0, 0}))
+
+	expected := strconv.Quote(`[0 0 6 6]
+`)
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		t.Errorf(expected)
+		t.Errorf(actual)
+	}
+}
+
+func TestBubbleLongArray(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	fmt.Fprintln(out, bubble([]int{7, -23, 70, 52, -12, 111, -6, 54, 33, -29, 0}))
+
+	expected := strconv.Quote(`[-29 -23 -12 -6 0 7 33 52 54 70 111]
 `)
 	actual := strconv.Quote(buf.String())
 
