4cb4c882a9ecda280c55dbd39dbf355957a74fbc
Cameron Schafer <cameron.schafer@anz.com>
2019-05-29 17:47:00 +1000

    Add bubbleLoop function Call bubbleLoop function in bubble function Add bubbleLoop test case to main_test.go

--- 02_bubble/CameronSchafer/main.go
+++ 02_bubble/CameronSchafer/main.go
@@ -9,7 +9,24 @@ import (
 var out io.Writer = os.Stdout
 
 func bubble(s []int) []int {
+	//testing some bubble sorting ideas
+	newS, check := s, false
+
+	for !check {
+		newS, check = bubbleLoop(newS)
+	}
+
 	return s
+}
+
+//this function loops through the slice and bubble sorts it.
+func bubbleLoop(nextS []int) ([]int, bool) {
+	bubbleCheck := true
+	newS := nextS
+
+	//bubbleCheck = true
+
+	return newS, bubbleCheck
 }
 
 func main() {
--- 02_bubble/CameronSchafer/main_test.go
+++ 02_bubble/CameronSchafer/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"strconv"
 	"testing"
 )
@@ -18,5 +19,38 @@ func TestMainOutput(t *testing.T) {
 
 	if expected != actual {
 		t.Errorf("Unexpected output in main()\nexpected: %q\nactual: %q", expected, actual)
+	}
+}
+
+//main() test
+func TestBubbleLoopOutput(t *testing.T) {
+	//test cases with descriptions.
+	testCases := []struct {
+		description  string
+		input        []int
+		expectedArr  []int
+		expectedBool bool
+	}{
+		{description: "bubbleLoop []int{3, 2, 1, 5}", input: []int{3, 2, 1, 5},
+			expectedArr: []int{3, 2, 1, 5}, expectedBool: true,
+		},
+		{description: "bubbleLoop []int{6,3,19}", input: []int{6, 3, 19},
+			expectedArr: []int{6, 3, 19}, expectedBool: true,
+		},
+		{description: "bubbleLoop []int{0,1,2}", input: []int{0, 1, 2},
+			expectedArr: []int{0, 1, 2}, expectedBool: true,
+		},
+	}
+
+	for _, test := range testCases {
+		test := test
+		// t.Run creates a sub test and runs it like a normal test
+		t.Run(test.description, func(t *testing.T) {
+			resultArr, resultBool := bubbleLoop(test.input)
+			if !reflect.DeepEqual(resultArr, test.expectedArr) || resultBool != test.expectedBool {
+				t.Errorf("Unexpected output in %v\nexpected: (%v,%v)\nactual: (%v,%v)",
+					test.description, test.expectedArr, test.expectedBool, resultArr, resultBool)
+			}
+		})
 	}
 }
