36888bc6e49515ad28f52350411b50ac512d9bbb
Choo, Hwan-Yi <Hwan-Yi.Choo@anz.com>
2019-05-29 23:30:40 +1000

    Code switches 3 numbers

--- 02_bubble/choohy/main.go
+++ 02_bubble/choohy/main.go
@@ -13,23 +13,50 @@ func main() {
 	//fmt.Fprintln(out, "[1 2]")
 
 	//fmt.Fprintln(out, bubble([]int{3, 2, 1, 5}))
-	// fmt.Fprintln(out, bubble([]int{2, 1}))
-	fmt.Fprintln(out, switchInt([]int{2, 1}))
+	fmt.Fprintln(out, bubble([]int{3, 2, 1, 5}))
+	//fmt.Fprintln(out, switchInt([]int{2, 1}))
+
+	a, b := swapInt(2, 1)
+	fmt.Println(a, b)
 }
 
 func bubble(s []int) []int {
-	for 
-	if (s[0]>s[1])
-		s[0], s[1] = switchInt(s[0],s[1])
+
+	//fmt.Println(len(s))
+
+	for i := 0; i < len(s); i++ {
+		next := i + 1
+		fmt.Println("i", i, "next", next, "s", s)
+		if next < len(s) && s[i] > s[next] {
+			a, b := swapInt(s[i], s[next])
+			s[i] = a
+			s[next] = b
+		}
+	}
+	// 	newBubble = make([]int, s.length)
+
+	// 	for i := 0; i < s.length; i++ {
+	// 	if (s[0]>s[1])
+	// 		currentBubble := switchInt([]int{2, 1})
+	// 	}
+	// 	fmt.Println("sum 1:", s)
+
+	// 	// if (s[0]>s[1])
+	// 	// 	s[0], s[1] = switchInt(s[0],s[1])
+	// 	return s
+	// 	//return int{s[1], s[0]}
 	return s
-	//return int{s[1], s[0]}
 }
 
-func switchInt(s []int) []int {
-	var a = s[0]
-	var b = s[1]
+// func switchInt(s []int) ([]int) {
+// 	var a = s[0]
+// 	var b = s[1]
 
-	//var t []int
-	t := []int{b, a}
-	return t
+// 	//var t []int
+// 	t := []int{b, a}
+// 	return t
+// }
+
+func swapInt(x, y int) (int, int) {
+	return y, x
 }
--- 02_bubble/choohy/main_test.go
+++ 02_bubble/choohy/main_test.go
@@ -6,16 +6,19 @@ import (
 	"testing"
 )
 
-func TestMainOutput(t *testing.T) {
+func TestBubble(t *testing.T) {
 	var buf bytes.Buffer
 	out = &buf
 
 	main()
 
-	expected := strconv.Quote("Bubble Sort!\n[1 2]\n")
+	expected := strconv.Quote(`Bubble Sort!
+[1 2 3 5]
+`)
 	actual := strconv.Quote(buf.String())
 
 	if expected != actual {
-		t.Errorf("Unexpected output in main()")
+		t.Errorf(expected)
+		t.Errorf(actual)
 	}
 }
