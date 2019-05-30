a6e915af357fa0e1f6682323febe9906b1071acf
eastrd <yjia285@student.monash.edu>
2019-05-30 11:59:32 +1000

    Add letters and sortLetters functions

---
+++ 03_letters/eastrd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func letters(s string) map[rune]int {
+	/*
+		Returns a mapping of each letter to its frequency
+	*/
+	dict := make(map[rune]int, 0)
+	for _, c := range s {
+		_, has := dict[c]
+		if has {
+			dict[c]++
+		} else {
+			dict[c] = 1
+		}
+	}
+	return dict
+}
+
+func sortLetters(m map[rune]int) []string {
+	/*
+		Returns a sorted slice of strings with elements "key:val"
+	*/
+	// Flatten map into a string
+	s := make([]string, len(m))
+	for k, v := range m {
+		s = append(s, string(k)+":"+strconv.Itoa(v))
+	}
+	return sort.StringSlice(s)
+}
+
+var out io.Writer = os.Stdout
+
+func main() {
+	fmt.Fprintln(out, strings.Join(sortLetters(letters("aba")), "\n"))
+}
