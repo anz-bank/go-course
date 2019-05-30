386eca0eca138a5531885e1a74b1cce28582e13d
Li, Luliang <luliang.li@anz.com>
2019-05-30 11:58:48 +1000

    fix the variable on range scope issue

--- 01_fib/luliangLi/main_test.go
+++ 01_fib/luliangLi/main_test.go
@@ -10,21 +10,18 @@ func TestMainOutput(t *testing.T) {
 	var buf bytes.Buffer
 	out = &buf
 
-	m_input_output := make(map[int]string)
-
-	m_input_output[7] = "1\n1\n2\n3\n5\n8\n13\n"
-	m_input_output[-7] = "0\n1\n-1\n2\n-3\n5\n-8\n13\n"
-
 	testCases := map[string]struct {
 		input int
 		want  string
 	}{
 		"fib":     {input: 7, want: "1\n1\n2\n3\n5\n8\n13\n"},
-		"negafib":      {input: -7, want: "0\n1\n-1\n2\n-3\n5\n-8\n13\n"},
+		"negafib": {input: -7, want: "0\n1\n-1\n2\n-3\n5\n-8\n13\n"},
 	}
 
 	for name, test := range testCases {
 		// t.Run creates a sub test and runs it like a normal test
+		test := test
+
 		t.Run(name, func(t *testing.T) {
 			fib(test.input)
 
