aeb6df69790e1ef5ecf49f2f236e08b963a3d3f2
Shahriyar, Shampa <Shampa.Shahriyar@anz.com>
2019-05-30 10:29:57 +1000

    100% test coverage ensured

--- 01_fib/shahriys/main_test.go
+++ 01_fib/shahriys/main_test.go
@@ -5,6 +5,19 @@ import (
 	"testing"
 )
 
+func TestMainOutput(t *testing.T) {
+	var buffib bytes.Buffer
+	outfib = &buffib
+
+	main()
+
+	expected := "1\n1\n2\n3\n5\n8\n13\n1\n-1\n2\n-3\n5\n-8\n13\n"
+	actual := buffib.String()
+
+	if expected != actual {
+		t.Errorf("Unexpected output in main()")
+	}
+}
 func TestFibPos(t *testing.T) {
 	var buffib bytes.Buffer
 	outfib = &buffib
