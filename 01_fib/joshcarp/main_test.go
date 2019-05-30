179a7827e8bda47f37e9c72cf8dd8b93d037af57
joshcarp <joshuacarpeggiani@gmail.com>
2019-05-31 09:22:01 +1000

    Add map variable for testing

--- 01_fib/joshcarp/main_test.go
+++ 01_fib/joshcarp/main_test.go
@@ -11,15 +11,15 @@ import (
 )
 
 func TestFib(t *testing.T) {
-	test1 := "1\n1\n2\n3\n5\n8\n13\n"
-	test2 := "1\n-1\n2\n-3\n5\n-8\n13\n"
+	tests := map[int]string{7: "1\n1\n2\n3\n5\n8\n13\n",
+		-7: "1\n-1\n2\n-3\n5\n-8\n13\n"}
 
-	re := captureStdout(fib, 7)
-	fmt.Printf(re)
-	require.Equal(t, re, test1)
-	re = captureStdout(fib, -7)
-	fmt.Printf(re)
-	require.Equal(t, re, test2)
+	for input, output := range tests {
+		re := captureStdout(fib, input)
+		fmt.Printf(re)
+		require.Equal(t, re, output)
+	}
+	main()
 	fmt.Println("exit")
 }
 
