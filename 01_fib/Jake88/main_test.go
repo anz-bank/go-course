069f3b6471ba18bf2c2182d13233e858f0a95744
Jake Turner <jake.turner@anz.com>
2019-05-31 12:12:36 +1000

    test(lab1): Test fib function
    Add a series of tests to ensure the fibonacci function outputs correctly if given both positive and negative integers.

--- 01_fib/Jake88/main_test.go
+++ 01_fib/Jake88/main_test.go
@@ -19,3 +19,31 @@ func TestMainOutput(t *testing.T) {
 		t.Errorf("Unexpected output printed by main.\n(Expected) : %v\n(Actual)   : %v", expected, actual)
 	}
 }
+
+func TestFib(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	type test struct {
+		input    int
+		expected string
+	}
+
+	tests := []test{
+		{-7, `"1\n-1\n2\n-3\n5\n-8\n13\n"`},
+		{7, `"1\n1\n2\n3\n5\n8\n13\n"`},
+		{-3, `"1\n-1\n2\n"`},
+		{4, `"1\n1\n2\n3\n"`},
+	}
+
+	for _, test := range tests {
+		fib(test.input)
+		actual := strconv.Quote(buf.String())
+
+		if test.expected != actual {
+			t.Errorf("Unexpected output printed by fib.\n(Expected) : %v\n(Actual)   : %v", test.expected, actual)
+		}
+
+		buf.Reset()
+	}
+}
