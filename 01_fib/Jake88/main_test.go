db6b347bd5a4b566d39d2b1363122f8667a22455
Jake Turner <jake.turner@anz.com>
2019-05-31 12:11:17 +1000

    test(lab1): Test main function
    Test written for the main function of fibonacci, lab1

---
+++ 01_fib/Jake88/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bytes"
+	"strconv"
+	"testing"
+)
+
+func TestMainOutput(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	main()
+
+	expected := strconv.Quote("1\n1\n2\n3\n5\n8\n13\n")
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		t.Errorf("Unexpected output printed by main.\n(Expected) : %v\n(Actual)   : %v", expected, actual)
+	}
+}
