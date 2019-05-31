46c94b2a455ba181e5b2775f59dcc1de563ca0f0
Chen, Shu <Shu.Chen@anz.com>
2019-05-31 16:49:36 +1000

    Shuutech fibonacci lab 1 fixed more  issues from golint

--- 01_fib/shuutech/main.go
+++ 01_fib/shuutech/main.go
@@ -11,10 +11,10 @@ var arrayList = []int{0, 1}
 
 func nextVal(a int, b int, n int, c int) []int {
 	numPair := []int{a, b}
-	var i int = numPair[0] + numPair[1]
-	var j int = numPair[1] + i
+	var i = numPair[0] + numPair[1]
+	var j = numPair[1] + i
 	newPair := []int{i, j}
-	c = c + 1
+	c += 1
 	if c < n {
 		arrayList = append(arrayList, newPair...)
 		nextVal(i, j, n, c)
@@ -26,7 +26,7 @@ func nextVal(a int, b int, n int, c int) []int {
 }
 
 func fib(n int) []int {
-	var slice2 []int = arrayList[:n]
+	var slice2 = arrayList[:n]
 
 	return slice2
 }
