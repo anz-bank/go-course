32ac6c1fb3f477603171d1c820d06628bd12eded
joshcarp <joshuacarpeggiani@gmail.com>
2019-05-31 16:20:22 +1000

    Add insertion sort

--- 02_bubble/joshcarp/bubble.go
+++ 02_bubble/joshcarp/bubble.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println(bubble([]int{3, 2, 1, 5}))
+	fmt.Println(insertion([]int{3, 2, 1, 5}))
 }
+
 func bubble(s []int) []int {
 	n := len(s)
 	for i := 0; i < n; i++ {
@@ -13,6 +15,20 @@ func bubble(s []int) []int {
 				s[j], s[j+1] = s[j+1], s[j]
 			}
 		}
+	}
+	return s
+}
+
+func insertion(s []int) []int {
+	i := 1
+	n := len(s)
+	for i < n {
+		j := i
+		for j > 0 && s[j-1] > s[j] {
+			s[j], s[j-1] = s[j-1], s[j]
+			j--
+		}
+		i++
 	}
 	return s
 }
