7e163b081aabb4375cf2bf43679d2c687622df5b
Kasun <Kasun.Fernando@anz.com>
2019-05-31 14:05:26 +1000

    Improved unit-tests

--- 02_bubble/kasunfdo/main_test.go
+++ 02_bubble/kasunfdo/main_test.go
@@ -7,6 +7,18 @@ import (
 	"testing"
 )
 
+var tests = map[string]struct {
+	input    []int
+	expected []int
+}{
+	"Empty array":                 {input: []int{}, expected: []int{}},
+	"Single element array":        {input: []int{101}, expected: []int{101}},
+	"Best case (sorted)":          {input: []int{1, 2, 3, 4, 5, 6, 7}, expected: []int{1, 2, 3, 4, 5, 6, 7}},
+	"Worst Case (reverse sorted)": {input: []int{7, 6, 5, 4, 3, 2, 1}, expected: []int{1, 2, 3, 4, 5, 6, 7}},
+	"Average Case (random order)": {input: []int{59, 62, 48, 53, 100, 86, 10, 68, 50, 9, 2},
+		expected: []int{2, 9, 10, 48, 50, 53, 59, 62, 68, 86, 100}},
+}
+
 func TestMainOutput(t *testing.T) {
 	var buf bytes.Buffer
 	out = &buf
@@ -21,24 +33,38 @@ func TestMainOutput(t *testing.T) {
 	}
 }
 
+func TestBubbleSort(t *testing.T) {
+	for name, test := range tests {
+		testData := test
+		t.Run(name, func(t *testing.T) {
+			actual := bubble(testData.input)
+			if !reflect.DeepEqual(testData.expected, actual) {
+				t.Errorf("Unexpected output in main()\nexpected: %d\nactual: %d", testData.expected, actual)
+			}
+		})
+	}
+}
+
 func TestInsertionSort(t *testing.T) {
-	input := []int{3, 2, 1, 5}
-
-	expected := []int{1, 2, 3, 5}
-	actual := insertionSort(input)
-
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("Unexpected output in main()\nexpected: %q\nactual: %q", expected, actual)
+	for name, test := range tests {
+		testData := test
+		t.Run(name, func(t *testing.T) {
+			actual := insertionSort(testData.input)
+			if !reflect.DeepEqual(testData.expected, actual) {
+				t.Errorf("Unexpected output in main()\nexpected: %d\nactual: %d", testData.expected, actual)
+			}
+		})
 	}
 }
 
 func TestMergeSort(t *testing.T) {
-	input := []int{3, 2, 1, 5}
-
-	expected := []int{1, 2, 3, 5}
-	actual := MergeSort(input)
-
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("Unexpected output in main()\nexpected: %q\nactual: %q", expected, actual)
+	for name, test := range tests {
+		testData := test
+		t.Run(name, func(t *testing.T) {
+			actual := MergeSort(testData.input)
+			if !reflect.DeepEqual(testData.expected, actual) {
+				t.Errorf("Unexpected output in main()\nexpected: %d\nactual: %d", testData.expected, actual)
+			}
+		})
 	}
 }
