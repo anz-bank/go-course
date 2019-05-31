06494d582ea4e08eb780afb87eaddecef15d0271
Shahriyar, Shampa <Shampa.Shahriyar@anz.com>
2019-05-31 12:55:23 +1000

    Complete Lab 3 Implement function that takes a string as input and returns a mapping of each letter to its frequency.

---
+++ 03_letters/shahriys/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var outletter io.Writer = os.Stdout
+
+func letters(s string) map[rune]int {
+
+	alphamap := make(map[rune]int)
+
+	for i := 0; i < len(s); i++ {
+		if val, ok := alphamap[rune(s[i])]; ok {
+			alphamap[rune(s[i])] = val + 1
+		} else {
+			alphamap[rune(s[i])] = 1
+		}
+
+	}
+	//fmt.Println(alphamap)
+	return alphamap
+}
+
+// As rune type can't be sorted so creating new type
+type RuneSlice []rune
+
+func (p RuneSlice) Len() int           { return len(p) }
+func (p RuneSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p RuneSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+func sortLetters(m map[rune]int) []string {
+
+	var keys []rune
+	for key := range m {
+		keys = append(keys, key)
+	}
+	var runes RuneSlice = keys
+	sort.Sort(runes)
+	var returnval []string
+
+	for _, k := range runes {
+		s := string(k) + ":" + strconv.FormatInt(int64(m[k]), 10)
+		returnval = append(returnval, s)
+	}
+	//fmt.Printf("%v  %T", returnval, returnval)
+	return returnval
+}
+func main() {
+	fmt.Fprint(outletter, strings.Join(sortLetters(letters("aba")), "\n"))
+}
---
+++ 03_letters/shahriys/main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestSortLettersMain(t *testing.T) {
+	var bufletter bytes.Buffer
+	outletter = &bufletter
+
+	main()
+	expected := `a:2
+b:1`
+	actual := bufletter.String()
+
+	if expected != actual {
+		t.Errorf("Unexpected output in main()" + expected + " " + actual)
+	}
+
+}
+func TestSortLetters(t *testing.T) {
+	var bufletter bytes.Buffer
+	outletter = &bufletter
+
+	type test struct {
+		input    string
+		expected []string
+		actual   []string
+	}
+
+	tests := []test{
+		{input: "abac", expected: []string{"a:2", "b:1", "c:1"}, actual: sortLetters(letters("abac"))},
+		{input: "aAdbbDA", expected: []string{"A:2", "D:1", "a:1", "b:2", "d:1"}, actual: sortLetters(letters("aAdbbDA"))},
+		{input: "aAAADDDbacZZZ", expected: []string{"A:3", "D:3", "Z:3", "a:2", "b:1", "c:1"}, actual: sortLetters(letters("aAAADDDbacZZZ"))},
+		{input: "", expected: []string{}, actual: sortLetters(letters(""))},
+	}
+
+	for _, tc := range tests {
+		if len(tc.expected) > 0 && len(tc.actual) > 0 {
+			if !reflect.DeepEqual(tc.expected, tc.actual) {
+				fmt.Printf("%v  %T", tc.expected, tc.expected)
+				fmt.Printf("%v  %T", tc.actual, tc.actual)
+				t.Fatalf("expected: %v, got: %v", tc.expected, tc.actual)
+			}
+		}
+	}
+
+}
