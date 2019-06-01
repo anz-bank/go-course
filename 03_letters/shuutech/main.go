d2799185de442d7dcf17c392b5170f870e5e8ebc
Chen, Shu <Shu.Chen@anz.com>
2019-06-01 22:50:29 +1000

    lab 3 fixed linting issues

--- 03_letters/shuutech/main.go
+++ 03_letters/shuutech/main.go
@@ -10,6 +10,7 @@ import (
 func letters(s string) map[rune]int {
 	var lettersMap = make(map[rune]int)
 	for i, r := range s {
+		i = 0
 		i = strings.Count(s, string(r))
 		lettersMap[r] = i
 	}
@@ -17,7 +18,7 @@ func letters(s string) map[rune]int {
 }
 
 func sortLetters(m map[rune]int) []string {
-	var stringKey []string
+	stringKey := []string{}
 	var s string
 	for key, value := range m {
 		s = string(key) + ":" + strconv.Itoa(value)
