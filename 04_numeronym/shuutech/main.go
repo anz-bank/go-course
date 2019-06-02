5a8c8f8a4ee5ce5ea8380dd298b4c484749d8d47
Chen, Shu <Shu.Chen@anz.com>
2019-06-02 17:26:53 +1000

    fixed linting issue

--- 04_numeronym/shuutech/main.go
+++ 04_numeronym/shuutech/main.go
@@ -1,28 +1,29 @@
 package main
-import(
+
+import (
 	"fmt"
 	"strconv"
 )
-func numeronyms(vals ...string) []string{
+
+func numeronyms(vals ...string) []string {
 	numeronym := []string{}
 	var stringToConvert string
 	for i := range vals {
-		if (len(vals[i])<=3){
+		if len(vals[i]) <= 3 {
 			numeronym = append(numeronym, vals[i])
 		}
-		if (len(vals[i])>3) {
-		stringToConvert = vals[i]
-		numeronym = append(numeronym,
-			string(stringToConvert[0]) + 
-			strconv.Itoa(len(stringToConvert)-2) +
-			string(stringToConvert[len(stringToConvert)-1]))
-	}
-		
-		i ++
+		if len(vals[i]) > 3 {
+			stringToConvert = vals[i]
+			numeronym = append(numeronym,
+				string(stringToConvert[0])+
+					strconv.Itoa(len(stringToConvert)-2)+
+					string(stringToConvert[len(stringToConvert)-1]))
+		}
+
 	}
 	return numeronym
 }
 
-func main(){
-fmt.Println(numeronyms("accessibility", "Kubernetes", "abc"))
+func main() {
+	fmt.Println(numeronyms("accessibility", "Kubernetes", "abc"))
 }
--- 04_numeronym/shuutech/main_test.go
+++ 04_numeronym/shuutech/main_test.go
@@ -1,28 +1,28 @@
 package main
+
 import (
-	"testing"
 	"fmt"
 	"os"
+	"testing"
 )
 
-func TestNumeronym(t *testing.T){
+func TestNumeronym(t *testing.T) {
 	expectedSlice := []string{"a11y", "abc"}
-	actualSlice := numeronyms("accessibility","abc")
+	actualSlice := numeronyms("accessibility", "abc")
 
 	var expected string
 	var actual string
 
-	for i := 0; i < 2; i++{
-		if (expectedSlice[i] != actualSlice[i]){
-				t.Errorf("expected %v, got %v", actual, expected)
-				t.Fail()
+	for i := 0; i < 2; i++ {
+		if expectedSlice[i] != actualSlice[i] {
+			t.Errorf("expected %v, got %v", actual, expected)
+			t.Fail()
 		}
 
 	}
 }
-	func TestMain(m *testing.M) {
-		main()
-		fmt.Printf("test!")
-		os.Exit(m.Run())
-	}
-
+func TestMain(m *testing.M) {
+	main()
+	fmt.Printf("test!")
+	os.Exit(m.Run())
+}
