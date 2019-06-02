7dc999aa3ce6fe29002a168cead88285904b06b7
David Jimenez <runnerdave@gmail.com>
2019-06-03 07:12:26 +1000

    Implement table unit testing

--- 01_fib/runnerdave/main_test.go
+++ 01_fib/runnerdave/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -29,42 +30,30 @@ func TestMainOutput(t *testing.T) {
 	}
 }
 
-func TestNegaFiboOutput(t *testing.T) {
-	var buf bytes.Buffer
-	out = &buf
-
-	fib(-7)
-
-	expected := strconv.Quote(`1
--1
-2
--3
-5
--8
-13
-`)
-	actual := strconv.Quote(buf.String())
-	t.Logf("expected:%s", expected)
-	t.Logf("actual:%s", actual)
-
-	if expected != actual {
-		t.Errorf("Unexpected output in main()")
+func TestFibo(t *testing.T) {
+	tables := []struct {
+		x int
+		n string
+	}{
+		{0, "0 "},
+		{1, "1 "},
+		{7, "1 1 2 3 5 8 13 "},
+		{2, "1 1 "},
+		{3, "1 1 2 "},
+		{6, "1 1 2 3 5 8 "},
+		{-6, "1 -1 2 -3 5 -8 "},
+		{-3, "1 -1 2 "},
+		{-4, "1 -1 2 -3 "},
 	}
-}
 
-func TestZeroOutput(t *testing.T) {
-	var buf bytes.Buffer
-	out = &buf
-
-	fib(0)
-
-	expected := strconv.Quote(`0
-`)
-	actual := strconv.Quote(buf.String())
-	t.Logf("expected:%s", expected)
-	t.Logf("actual:%s", actual)
-
-	if expected != actual {
-		t.Errorf("Unexpected output in main()")
+	for _, table := range tables {
+		var buf bytes.Buffer
+		out = &buf
+		fib(table.x)
+		expected := strconv.Quote(table.n)
+		actual := strconv.Quote(strings.Replace(buf.String(), "\n", " ", -1))
+		if expected != actual {
+			t.Errorf("Fibo of (%d) was incorrect, got: %s, want: %s.", table.x, actual, expected)
+		}
 	}
 }
