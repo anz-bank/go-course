75aa43b74c54b41d4ec315a73cb44e698eb84df7
Jha, Ranjana <Ranjana.Jha@anz.com>
2019-06-03 13:57:56 +1000

    fixed lint error

--- 01_fib/anzranjana/main.go
+++ 01_fib/anzranjana/main.go
@@ -10,31 +10,33 @@ var out io.Writer = os.Stdout
 
 func Fib(number int) {
 	firstNum, nextNum := int64(0), int64(1)
-
-	if number == 0 {
+	switch number {
+	case 0:
 		fmt.Fprintln(out, number)
-	} else if number < 0 { // Negative Fibonacci series
-		for i := -1; i >= number; i-- {
-			firstNum, nextNum = nextNum, firstNum-nextNum
-			fmt.Fprintln(out, firstNum)
-			if i <= -92 { //-92 is the last good value for 64 bit int
-				fmt.Fprintln(out, "max fibo series reached")
-				break
+	default:
+		if number < 0 { // Negative Fibonacci series
+			for i := -1; i >= number; i-- {
+				firstNum, nextNum = nextNum, firstNum-nextNum
+				fmt.Fprintln(out, firstNum)
+				if i <= -92 { //-92 is the last good value for 64 bit int
+					fmt.Fprintln(out, "max fibo series reached")
+					break
+				}
+			}
+		} else { // positive Fibonacci series
+			for i := 1; i <= number; i++ {
+				firstNum, nextNum = nextNum, firstNum+nextNum
+				fmt.Fprintln(out, firstNum)
+				if i >= 92 { //92 is the last good value for 64 bit int
+					fmt.Fprintln(out, "max fibo series reached")
+					break
+				}
 			}
 		}
-	} else { // positive Fibonacci series
-		for i := 1; i <= number; i++ {
-			firstNum, nextNum = nextNum, firstNum+nextNum
-			fmt.Fprintln(out, firstNum)
-			if i >= 92 { //92 is the last good value for 64 bit int
-				fmt.Fprintln(out, "max fibo series reached")
-				break
-			}
-		}
+
 	}
 }
 
 func main() {
 	Fib(7)
-
 }
