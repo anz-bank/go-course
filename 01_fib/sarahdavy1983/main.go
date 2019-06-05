7f89f2df974e7ee117e1bcd8d1c844ff768a31f1
Sarah Davy <sarah.davy@anz.com>
2019-06-05 10:48:58 +1000

    Fibonacci nega & positive function incl testing

---
+++ 01_fib/sarahdavy1983/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// Write to this rather that Stdout directly to help with testing
+var out io.Writer = os.Stdout
+
+// fib returns a slice of integer fibonacci numbers
+func fib(n int) []int64 {
+	switch {
+	case n == 0:
+		return []int64{0, 0}
+	case n == 1:
+		return []int64{0, 1}
+	case n == -1:
+		return []int64{0, 1}
+	case n < -1:
+		x := fib(n + 1)
+		nextVal := x[len(x)-2] - x[len(x)-1]
+		return append(x, nextVal)
+
+	default:
+		x := fib(n - 1)
+		nextVal := x[len(x)-2] + x[len(x)-1]
+		return append(x, nextVal)
+	}
+
+}
+
+//printEachindex iterates over the slice and prints each index
+func printEachIndex(fibSlice []int64) {
+	for i := 1; i < len(fibSlice); i++ {
+		fmt.Fprintln(out, fibSlice[i])
+	}
+}
+func main() {
+	printEachIndex(fib(7))
+}
---
+++ 01_fib/sarahdavy1983/main_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+func TestMainOutput(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	main()
+
+	expected := strconv.Quote(`1
+1
+2
+3
+5
+8
+13
+`)
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		t.Errorf("Unexpected output in main(), expected = %v, actual = %v", expected, actual)
+	}
+}
+
+func TestFibVal(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+	type test struct {
+		want  []int64
+		input int
+	}
+
+	tests := []test{
+		{input: -7, want: []int64{0, 1, -1, 2, -3, 5, -8, 13}},
+		{input: 0, want: []int64{0, 0}},
+		{input: 1, want: []int64{0, 1}},
+		{input: 7, want: []int64{0, 1, 1, 2, 3, 5, 8, 13}},
+		{input: -1, want: []int64{0, 1}},
+	}
+
+	for _, tc := range tests {
+
+		got := (fib(tc.input))
+		//strconv.Quote(buf.String())
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("want: %v, got: %v", tc.want, got)
+		}
+	}
+}
