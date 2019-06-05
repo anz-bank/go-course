e967b9cd7a4709cbdb6afcc8115dd0c27ce4c19b
David Jimenez <runnerdave@gmail.com>
2019-06-05 22:03:22 +1000

    Implement feedback from PR review

--- 02_bubble/runnerdave/main.go
+++ 02_bubble/runnerdave/main.go
@@ -13,15 +13,15 @@ func main() {
 }
 
 func bubble(s []int) []int {
-	len := len(s)
+	sorted := make([]int, len(s))
+	copy(sorted, s)
+	len := len(sorted)
 	for i := 0; i < len-1; i++ {
 		for j := 0; j < len-i-1; j++ {
-			if s[j] > s[j+1] {
-				temp := s[j]
-				s[j] = s[j+1]
-				s[j+1] = temp
+			if sorted[j] > sorted[j+1] {
+				sorted[j], sorted[j+1] = sorted[j+1], sorted[j]
 			}
 		}
 	}
-	return s
+	return sorted
 }
