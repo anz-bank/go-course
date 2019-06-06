0be58b58f408b8bdeb3ad82d9d1cbdede26d084a
lexual <Alexei.Hider@anz.com>
2019-06-06 20:58:41 +1000

    Make the IPAddr type implement fmt.Stringer

---
+++ 05_stringer/lexual/stringer_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"strconv"
+	"testing"
+)
+
+func TestMainOutput(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	main()
+
+	expected := strconv.Quote("127.0.0.1\n")
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		t.Errorf("Unexpected output in main()")
+		t.Errorf("\nActual: %s\nExpected: %s", actual, expected)
+	}
+}
