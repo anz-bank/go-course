48d6eac4c20c9197745963e5f6fe6a838cdc3386
Jake Turner <jake.turner@anz.com>
2019-06-06 10:10:57 +1000

    refactor(fib): Refactor fib function to use two variables instead of a map
    Removed the map from fib function and replaced it with two integers to save memory.

--- 01_fib/Jake88/main.go
+++ 01_fib/Jake88/main.go
@@ -12,35 +12,33 @@ func main() {
 	fib(7)
 }
 
-func fib(n int) {
-	// Our initial map, with the default starting values needed to begin the sequence.
-	fibonacciMap := map[int]int{
-		-1: 1,
-		0:  0,
-	}
-	negative := false
-
-	// Check if we are performaing negafibonacci or not, and if so remove the sign from n
+func absolute(n int) (int, bool) {
+	var negative bool
 	if n < 0 {
-		n *= -1
+		n = n * -1
 		negative = true
 	}
+	return n, negative
+}
 
-	// Form our map of fibonacci numbers, ensuring i matches Fn
-	for i := 1; i < n+1; i++ {
-		// Store the next positive fibonacci iteration
-		current := fibonacciMap[i-1] + fibonacciMap[i-2]
-		fibonacciMap[i] = current
+func fib(n int) {
 
-		// If negafibonacci, store the next negative iteration
-		if negative {
-			if i%2 == 0 {
-				current = -current
-			}
-			fibonacciMap[-i] = current
+	prev2, prev1 := 1, 0
+
+	n, nega := absolute(n)
+
+	for i := 0; i < n; i++ {
+		current := prev1 + prev2
+
 		}
 
-		// Print out the current iteration value
-		fmt.Fprintln(out, current)
+		prev2, prev1 = prev1, current
+
+		// print based on whether we are in negafibonacci or not
+		if nega && i%2 == 1 {
+			fmt.Fprintln(out, -current)
+		} else {
+			fmt.Fprintln(out, current)
+		}
 	}
 }
