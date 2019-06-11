ebfd3828bcab07588d977771bcc2448e97049860
Kai Zhu <Kai.Zhu@anz.com>
2019-06-11 12:21:42 +1000

    Write a function that prints out the first n Fibnacci numbers

---
+++ 01_fib/kaizhu/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var out io.Writer = os.Stdout
+
+func main() {
+	fib(7)
+}
+
+func fib(n int64) {
+
+	switch {
+	// print 0 if n is 0
+	case n == 0:
+		fmt.Fprintln(out, 0)
+	// print a message if n is too big or too small
+	case n < -92 || n > 92:
+		fmt.Fprintln(out, "Please enter a number within the range [-92, 92].")
+	// print fib array if n is within range
+	default:
+		// check if n is negative
+		var sign int64 = 1
+		if n < 0 {
+			n = 0 - n
+			sign = -1
+		}
+
+		// generate fib array
+		// fibArray := make([]int64, n)
+		var a, b int64 = 1, sign
+		var i int64
+		for i = 0; i < n; i++ {
+			fmt.Fprintln(out, a)
+			a, b = b, a+b*sign
+		}
+	}
+}
---
+++ 01_fib/kaizhu/main_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"strconv"
+	"testing"
+)
+
+func TestMainOutput(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	main()
+
+	expected := strconv.Quote(`1
+1
+2
+3
+5
+8
+13
+`)
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		t.Errorf("Unexpected output in main()\nexpected: %q\nactual: %q", expected, actual)
+	}
+}
+
+func TestFib(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+
+	type test struct {
+		input    int64
+		expected string
+	}
+
+	tests := []test{
+		{input: -7,
+			expected: strconv.Quote(`1
+-1
+2
+-3
+5
+-8
+13
+`)},
+		{input: -1,
+			expected: strconv.Quote("1\n")},
+		{input: 1,
+			expected: strconv.Quote("1\n")},
+		{input: 0,
+			expected: strconv.Quote("0\n")},
+		{input: 93,
+			expected: strconv.Quote("Please enter a number within the range [-92, 92].\n")},
+		{input: -93,
+			expected: strconv.Quote("Please enter a number within the range [-92, 92].\n")},
+	}
+
+	for _, test := range tests {
+		fib(test.input)
+		actual := strconv.Quote(buf.String())
+
+		if test.expected != actual {
+			t.Errorf("Unexpected output in main()\ninput: %d\nexpected: %q\nactual: %q", test.input, test.expected, actual)
+		}
+
+		buf.Reset()
+	}
+}
