9688e34ed23c3c86940c0c8825f9775260b36894
James Leonard <james.leonard2@anz.com>
2019-06-14 10:56:07 +1000

    Complete lab 1 fibonnaci

---
+++ 01_fib/leonarj4/main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestFib(t *testing.T){
+
+	for i:= 0; i <= 7; i++ {
+		
+		switch total := fib(i)
+		total {
+		case 1,3,5,7,9,11,13:
+			t.Logf("The test passed with current number: %d", total)
+		default:
+			t.Errorf("The test did not pass with current number: %d", total)
+		}
+	}
+}
