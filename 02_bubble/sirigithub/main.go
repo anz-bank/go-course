151c79887d0b38a86abf511c17a926ec325c4157
Sirisha Vadrevu <sirideals@gmail.com>
2019-06-14 13:00:29 +1000

    Implemented bubble sort and insertion sort with tests.

--- 02_bubble/sirigithub/main.go
+++ 02_bubble/sirigithub/main.go
@@ -8,7 +8,7 @@ import (
 
 var out io.Writer = os.Stdout
 
-func bubble(s []int) []int {
+func bubbleSort(s []int) []int {
 	n := len(s)
 	for i := 0; i < n; i++ {
 		for j :=1 ; j < n-i; j++ {
@@ -17,20 +17,16 @@ func bubble(s []int) []int {
 				s[j-1], s[j]= s[j], s[j-1]
 			}
 		}
-    fmt.Println("   ",s)
 	}
 	return s
 }
 
-func insertion(s []int) []int {
+func insertionSort(s []int) []int {
   i:= 1
 
   for i < len(s) {
-    j:= i
-
-    for j > 0 && s[j-1] > s[j] {
+    for j:= i;j > 0 && s[j-1] > s[j]; j-- {
       s[j-1] , s[j] = s[j], s[j-1]
-      j--
     }
     i++
   }
@@ -39,6 +35,6 @@ func insertion(s []int) []int {
 }
 
 func main() {
-	fmt.Fprintln(out, bubble([]int{3, 2, 1, 5}))
-  fmt.Fprintln(out, insertion([]int{8, 2, 1, 6}))
+	fmt.Fprintln(out,bubbleSort([]int{3, 2, 1, 5}))
+
 }
