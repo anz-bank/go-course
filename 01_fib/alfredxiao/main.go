1dfc261d4c41fa57566ddb28d746d749c08ad7c7
Alfred Xiao <xiaoyf@gmail.com>
2019-06-15 15:55:46 +1000

    Implement fibonacci series, tested edge cases and pos/negatives

--- 01_fib/alfredxiao/main.go
+++ 01_fib/alfredxiao/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
+var out io.Writer = os.Stdout
+
+func fibSeries(n int) []int {
+	var series []int
+	fa, fb := 0, 1
+	p, fn := 0, 0
+	for {
+		switch {
+		case p == 0:
+			fn = fa
+		case p == 1:
+			fn = fb
+		case p > 1:
+			fa, fb = fb, fa+fb
+			fn = fb
+		case p < 0:
+			fa, fb = fb-fa, fa
+			fn = fa
+		}
+		series = append(series, fn)
+		if p == n {
+			break
+		}
+		if n > 0 {
+			p++
+		} else if n < 0 {
+			p--
+		}
+	}
+	return series
+}
+
+func fib(n int) {
+	series := fibSeries(n)
+	for _, fn := range series {
+		fmt.Fprintln(out, fn)
+	}
+}
+
+func main() {
+	fib(7)
 }
