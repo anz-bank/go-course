4e1b4fcc464061c51d162a943b702a7ee12af5ad
Siyuan Hu <hsy3418@gmail.com>
2019-06-16 20:54:06 +1000

    Rewrite the fib method to make it more readable

--- 01_fib/hsy3418/main.go
+++ 01_fib/hsy3418/main.go
@@ -10,31 +10,11 @@ var out io.Writer = os.Stdout
 
 //fib is a function to print out a list of fibonacci number
 func fib(n int) {
-<<<<<<< HEAD
-
-	first := 1
-	second := 1
-	for i := 0; i < n; i++ {
-		switch i {
-		case 0:
-			fmt.Fprintln(out, first)
-		case 1:
-			fmt.Fprintln(out, second)
-		default:
-			current := first + second
-			first = second
-			second = current
-			fmt.Fprintln(out, current)
-		}
-
-=======
 	first, second := 1, 1
 	for i := 1; i <= n; i++ {
 		fmt.Fprintln(out, first)
 		first, second = second, first+second
->>>>>>> c86e684... rewrite fibonacci function
 	}
-
 }
 
 func main() {
--- 01_fib/hsy3418/main_test.go
+++ 01_fib/hsy3418/main_test.go
@@ -3,8 +3,45 @@ package main
 import (
 	"bytes"
 	"strconv"
+	"strings"
 	"testing"
 )
+
+func TestFib1Output(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+	fib(1)
+	expected := strconv.Quote(replaceSpaceWithlineBreak("1 "))
+	actual := strconv.Quote(buf.String())
+	if expected != actual {
+		t.Errorf("Unexpected output in main()\nexpected: %q\nactual: %q", expected, actual)
+	}
+
+}
+
+func TestFib2Output(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+	fib(2)
+	expected := strconv.Quote(replaceSpaceWithlineBreak("1 1 "))
+	actual := strconv.Quote(buf.String())
+	if expected != actual {
+		t.Errorf("Unexpected output in main()\nexpected: %q\nactual: %q", expected, actual)
+	}
+
+}
+
+func TestFib3Output(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+	fib(3)
+	expected := strconv.Quote(replaceSpaceWithlineBreak("1 1 2 "))
+	actual := strconv.Quote(buf.String())
+	if expected != actual {
+		t.Errorf("Unexpected output in main()\nexpected: %q\nactual: %q", expected, actual)
+	}
+
+}
 
 func TestMainOutput(t *testing.T) {
 	var buf bytes.Buffer
@@ -12,10 +49,15 @@ func TestMainOutput(t *testing.T) {
 
 	main()
 
-	expected := strconv.Quote("1\n1\n2\n3\n5\n8\n13\n")
+	expected := strconv.Quote(replaceSpaceWithlineBreak("1 1 2 3 5 8 13 "))
 	actual := strconv.Quote(buf.String())
 
 	if expected != actual {
-		t.Errorf("Unexpected output in main()")
+		t.Errorf("Unexpected output in main()\nexpected: %q\nactual: %q", expected, actual)
 	}
+
+}
+
+func replaceSpaceWithlineBreak(line string) string {
+	return strings.Replace(line, " ", "\n", -1)
 }
