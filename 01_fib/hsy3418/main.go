5ef8b236236d6a9443ee710fc57034669b9256cf
SIYUAN HU <hsy3418@gmail.com>
2019-06-16 14:52:10 +1000

    Fix code conflicts

--- 01_fib/hsy3418/main.go
+++ 01_fib/hsy3418/main.go
@@ -8,7 +8,9 @@ import (
 
 var out io.Writer = os.Stdout
 
+//fib is a function to print out a list of fibonacci number
 func fib(n int) {
+<<<<<<< HEAD
 
 	first := 1
 	second := 1
@@ -25,6 +27,12 @@ func fib(n int) {
 			fmt.Fprintln(out, current)
 		}
 
+=======
+	first, second := 1, 1
+	for i := 1; i <= n; i++ {
+		fmt.Fprintln(out, first)
+		first, second = second, first+second
+>>>>>>> c86e684... rewrite fibonacci function
 	}
 
 }
