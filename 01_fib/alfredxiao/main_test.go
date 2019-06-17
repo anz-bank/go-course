624d39ad26c8a90f4fe353f1f41e8709f67d3eee
Alfred Xiao <alfred.xiao@anz.com>
2019-06-17 11:41:12 +1000

    Test panic situation where number out of range

--- 01_fib/alfredxiao/main_test.go
+++ 01_fib/alfredxiao/main_test.go
@@ -23,6 +23,13 @@ func TestFibNegative(t *testing.T) {
 	assert.Equal(t, []int{0, 1, -1, 2, -3, 5, -8, 13}, fibSeries(-7))
 }
 
+func TestFibSeriesPanic(t *testing.T) {
+	assert.Panics(t, func() { fibSeries(93) }, "Panic when number of range")
+	assert.Panics(t, func() { fibSeries(94) }, "Panic when number of range")
+	assert.Panics(t, func() { fibSeries(-93) }, "Panic when number of range")
+	assert.Panics(t, func() { fibSeries(-94) }, "Panic when number of range")
+}
+
 func TestFib(t *testing.T) {
 	var buf bytes.Buffer
 	out = &buf
