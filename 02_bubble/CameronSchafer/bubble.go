166b2143dc855219f94c7a2b400ef81793c7e912
Cameron Schafer <cameron.schafer@anz.com>
2019-06-18 14:27:36 +1000

    Update test cases to use maps instead of structs Update variable names to be more informative Remove incorrect merge sort function

--- 02_bubble/CameronSchafer/bubble.go
+++ 02_bubble/CameronSchafer/bubble.go
@@ -15,7 +15,7 @@ func bubble(s []int) []int {
 	check := false
 
 	for !check {
-		tempCheck := true
+		swapped := true
 		// sort once and then check
 		for i := 0; i < len(t); i++ {
 			// check if in last slice
@@ -23,11 +23,11 @@ func bubble(s []int) []int {
 				if t[i] > t[i+1] {
 					// swap the elems around
 					t[i], t[i+1] = t[i+1], t[i]
-					tempCheck = false
+					swapped = false
 				}
 			}
 		}
-		check = tempCheck
+		check = swapped
 	}
 	return t
 }
@@ -49,50 +49,7 @@ func insertion(s []int) []int {
 	return s
 }
 
-// accidentally created an O(n2) complexity instead of O(n log n) complexity
-// merge sort: O(n2) sorting algorithm
-func merge(s []int) []int {
-	sBox := [][]int{}
-	for a := range s {
-		sBox = append(sBox, []int{s[a]})
-	}
-
-	for len(sBox) > 1 {
-		var tBox [][]int
-		for i := 0; i < len(sBox); i += 2 {
-			// if the adjacent box exists then compare them
-			if i+1 != len(sBox) {
-				tSlice := []int{}
-				// need to do for loop here to check if i and i+1 lengths are greater than 0
-				for len(sBox[i]) > 0 && len(sBox[i+1]) > 0 {
-					a, b := sBox[i][0], sBox[i+1][0]
-					if a < b {
-						tSlice = append(tSlice, a)
-						sBox[i] = sBox[i][1:]
-					} else {
-						tSlice = append(tSlice, b)
-						sBox[i+1] = sBox[i+1][1:]
-					}
-				}
-				// need to do a check here for which array has less values
-				if len(sBox[i]) > 0 {
-					tSlice = append(tSlice, sBox[i]...)
-				} else if len(sBox[i+1]) > 0 {
-					tSlice = append(tSlice, sBox[i+1]...)
-				}
-				tBox = append(tBox, tSlice)
-			} else {
-				tBox = append(tBox, sBox[i])
-			}
-		}
-		// set sBox to value of tBox
-		sBox = tBox
-	}
-	return sBox[0]
-}
-
 func main() {
 	fmt.Fprintln(out, bubble([]int{3, 2, 1, 5}))
 	fmt.Fprintln(out, insertion([]int{3, 2, 1, 5}))
-	fmt.Fprintln(out, merge([]int{3, 2, 1, 5}))
 }
--- 02_bubble/CameronSchafer/bubble_test.go
+++ 02_bubble/CameronSchafer/bubble_test.go
@@ -15,7 +15,7 @@ func TestMainOutput(t *testing.T) {
 
 	main()
 
-	expected := strconv.Quote("[1 2 3 5]\n[1 2 3 5]\n[1 2 3 5]\n")
+	expected := strconv.Quote("[1 2 3 5]\n[1 2 3 5]\n")
 	actual := strconv.Quote(buf.String())
 
 	if expected != actual {
@@ -25,30 +25,23 @@ func TestMainOutput(t *testing.T) {
 
 //bubble() test
 func TestBubbleOutput(t *testing.T) {
-	//test cases with descriptions.
-	testCases := []struct {
-		description string
+	testCases := map[string]struct {
 		input       []int
 		expectedArr []int
 	}{
-		{description: "bubble []int{3, 2, 1, 5}", input: []int{3, 2, 1, 5},
-			expectedArr: []int{1, 2, 3, 5},
-		},
-		{description: "bubble []int{6,3,19}", input: []int{7, -1, 10000},
-			expectedArr: []int{-1, 7, 10000},
-		},
-		{description: "bubble []int{0,1,2}", input: []int{-1, -30, -2},
-			expectedArr: []int{-30, -2, -1},
-		},
+		"{-20,-35}":         {input: []int{-20, -35}, expectedArr: []int{-35, -20}},
+		"{0,2,0,2}":         {input: []int{0, 2, 0, 2}, expectedArr: []int{0, 0, 2, 2}},
+		"{100,30000,29999}": {input: []int{100, 30000, 29999}, expectedArr: []int{100, 29999, 30000}},
 	}
 
-	for _, test := range testCases {
+	for name, test := range testCases {
 		test := test
+		name := name
 		// t.Run creates a sub test and runs it like a normal test
-		t.Run(test.description, func(t *testing.T) {
+		t.Run(name, func(t *testing.T) {
 			resultArr := bubble(test.input)
 			assert.Equalf(t, resultArr, test.expectedArr, "Unexpected output in %v\nexpected: %v,\nactual: %v",
-				test.description, test.expectedArr, resultArr)
+				name, test.expectedArr, resultArr)
 		})
 	}
 }
@@ -56,59 +49,25 @@ func TestBubbleOutput(t *testing.T) {
 //insertion() test
 func TestInsertionOutput(t *testing.T) {
 	//test cases with descriptions.
-	testCases := []struct {
-		description string
+	testCases := map[string]struct {
 		input       []int
 		expectedArr []int
 	}{
-		{description: "insertion []int{3, 2, 1, 5}", input: []int{3, 83, 103003, 50, 20, 29, 5218, -1295, 0},
-			expectedArr: []int{-1295, 0, 3, 20, 29, 50, 83, 5218, 103003},
-		},
-		{description: "insertion []int{6,3,19}", input: []int{6},
-			expectedArr: []int{6},
-		},
-		{description: "insertion []int{0,1,2}", input: []int{0, 1},
-			expectedArr: []int{0, 1},
-		},
-		{description: "insertion []int{0,1,2}", input: []int{-10, 1, -12, 2, 90, 0, -100},
-			expectedArr: []int{-100, -12, -10, 0, 1, 2, 90},
-		},
+		"{0}":          {input: []int{0}, expectedArr: []int{0}},
+		"{0, 0, 0}":    {input: []int{0, 0, 0}, expectedArr: []int{0, 0, 0}},
+		"{0,1,2}":      {input: []int{0, 1, 2}, expectedArr: []int{0, 1, 2}},
+		"{6,3,19}":     {input: []int{6, 3, 19}, expectedArr: []int{3, 6, 19}},
+		"{3, 2, 1, 5}": {input: []int{3, 2, 1, 5}, expectedArr: []int{1, 2, 3, 5}},
 	}
 
-	for _, test := range testCases {
+	for name, test := range testCases {
 		test := test
+		name := name
 		// t.Run creates a sub test and runs it like a normal test
-		t.Run(test.description, func(t *testing.T) {
+		t.Run(name, func(t *testing.T) {
 			resultArr := insertion(test.input)
 			assert.Equalf(t, resultArr, test.expectedArr, "Unexpected output in %v\nexpected: %v,\nactual: %v",
-				test.description, test.expectedArr, resultArr)
-		})
-	}
-}
-
-//merge() test
-func TestMergeOutput(t *testing.T) {
-	//test cases with descriptions.
-	testCases := []struct {
-		description string
-		input       []int
-		expectedArr []int
-	}{
-		{description: "merge []int{3, 2, 1, 5}", input: []int{-3, -2, -1, -5},
-			expectedArr: []int{-5, -3, -2, -1},
-		},
-		{description: "merge []int{0,1,2}", input: []int{-10, 1, -12, 2, 90, 0, -100, -10, 1, -12, 2, 90, 0, -100},
-			expectedArr: []int{-100, -100, -12, -12, -10, -10, 0, 0, 1, 1, 2, 2, 90, 90},
-		},
-	}
-
-	for _, test := range testCases {
-		test := test
-		// t.Run creates a sub test and runs it like a normal test
-		t.Run(test.description, func(t *testing.T) {
-			resultArr := merge(test.input)
-			assert.Equalf(t, resultArr, test.expectedArr, "Unexpected output in %v\nexpected: %v,\nactual: %v",
-				test.description, test.expectedArr, resultArr)
+				name, test.expectedArr, resultArr)
 		})
 	}
 }
