44b335eea7d5022cc621c9424e151bd1e30be148
Agung Haman <agung.haman@anz.com>
2019-06-20 16:22:11 +1000

    Added main that calls letters function to prints out letters and count
    - Had to use Fprint instead of println as per instruction as we want to test the output of the print outs
    - Added letters function takes a string and create a map of letter keys with counts as values
    - Added sortLetters function to take a map of rune keys with count and sort them based on rune value and return string slice
    - Added sortRune function to sort the rune keys based on its value
    - Added tests to cover all cases including nil, empty inputs

--- 03_letters/starfallz/main.go
+++ 03_letters/starfallz/main.go
@@ -4,10 +4,46 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 var out io.Writer = os.Stdout
 
 func main() {
-	fmt.Fprintln(out, "test letters")
+	fmt.Fprint(out, strings.Join(sortLetters(letters("aba")), "\n"))
+}
+
+func letters(s string) map[rune]int {
+	letterCount := make(map[rune]int)
+
+	for _, currentLetter := range s {
+		letterCount[currentLetter]++
+	}
+
+	return letterCount
+}
+
+func sortLetters(m map[rune]int) []string {
+	allKeys := make([]rune, 0, len(m))
+	for k := range m {
+		allKeys = append(allKeys, k)
+	}
+
+	sortRune(allKeys)
+
+	result := make([]string, len(allKeys))
+
+	for i, currentKey := range allKeys {
+		result[i] = strings.Join([]string{string(currentKey), strconv.Itoa(m[currentKey])}, ":")
+	}
+
+	return result
+}
+
+func sortRune(r []rune) {
+	sort.Slice(r, func(i, j int) bool {
+		return r[i] < r[j]
+	})
 }
--- 03_letters/starfallz/main_test.go
+++ 03_letters/starfallz/main_test.go
@@ -1,1 +1,103 @@
 package main
+
+import (
+	"bytes"
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+func TestMainFunction(t *testing.T) {
+	t.Run("Test main to return string values of letters(aba) with proper formatting", func(t *testing.T) {
+		var buf bytes.Buffer
+		out = &buf
+
+		main()
+
+		expected := strconv.Quote(`a:2
+b:1`)
+		actual := strconv.Quote(buf.String())
+
+		if expected != actual {
+			t.Errorf("Unexpected output, expected: %s, actual: %s", expected, actual)
+		}
+	})
+}
+
+func TestLettersFunction(t *testing.T) {
+	testCases := []struct {
+		description string
+		input       string
+		expected    map[rune]int
+	}{
+		{"Test letters to return sorted map of letters based on rune # with count", "abaabazzzzzz",
+			map[rune]int{97: 4, 98: 2, 122: 6}},
+		{"Test letters to handle uppercase letters", "aAAaA", map[rune]int{65: 3, 97: 2}},
+		{"Test letters to handle non-alphabetical characters", "    !!!*", map[rune]int{32: 4, 33: 3, 42: 1}},
+		{"Test letters to return empty map on empty string input", "", map[rune]int{}},
+	}
+
+	for _, testCase := range testCases {
+		input := testCase.input
+		expected := testCase.expected
+		t.Run(testCase.description, func(t *testing.T) {
+			actual := letters(input)
+
+			if !reflect.DeepEqual(actual, expected) {
+				t.Errorf("Unexpected output, expected: %d, actual: %d", expected, actual)
+			}
+		})
+	}
+}
+
+func TestSortLettersFunction(t *testing.T) {
+	testCases := []struct {
+		description string
+		input       map[rune]int
+		expected    []string
+	}{
+		{"Test sort letters using letters() as input and return count", letters("aba"), []string{"a:2", "b:1"}},
+		{"Test sort letters with uppercase using letters() as input and return count", letters("Aba"),
+			[]string{"A:1", "a:1", "b:1"}},
+		{"Test sort letters with uppercase and return count", map[rune]int{97: 2, 65: 3}, []string{"A:3", "a:2"}},
+		{"Test sort letters with empty slice", map[rune]int{}, []string{}},
+		{"Test nil input to output nil", nil, []string{}},
+	}
+
+	for _, testCase := range testCases {
+		input := testCase.input
+		expected := testCase.expected
+		t.Run(testCase.description, func(t *testing.T) {
+			actual := sortLetters(input)
+
+			if !reflect.DeepEqual(actual, expected) {
+				t.Errorf("Unexpected output, expected: %s, actual: %s", expected, actual)
+			}
+		})
+	}
+}
+
+func TestSortRuneFunction(t *testing.T) {
+	testCases := []struct {
+		description string
+		input       []rune
+		expected    []rune
+	}{
+		{"Test sort unsorted rune slice to sorted rune slice", []rune{97, 82}, []rune{82, 97}},
+		{"Test sort negative rune", []rune{-97, 82}, []rune{-97, 82}},
+		{"Test sort empty slice", []rune{}, []rune{}},
+		{"Test nil input to output nil", nil, nil},
+	}
+
+	for _, testCase := range testCases {
+		actual := testCase.input
+		expected := testCase.expected
+		t.Run(testCase.description, func(t *testing.T) {
+			sortRune(actual)
+
+			if !reflect.DeepEqual(actual, expected) {
+				t.Errorf("Unexpected output, expected: %d, actual: %d", expected, actual)
+			}
+		})
+	}
+}
