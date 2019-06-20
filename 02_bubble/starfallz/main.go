56ea83758ae169568eeba5028ba3fffe823378a4
Agung Haman <agung.haman@anz.com>
2019-06-20 15:28:57 +1000

    Added main that calls bubble function that output array of numbers in form of [1 2 3]
    - Had to use Fprint instead of println as per instruction as we want to test the output of the print outs
    - This is the most basic way of sorting with nested loop going through each number and swapping it with the next number
    - If there is no more swapping required after checking the nested loop, then break the parent loop so its slightly more efficient
    - Broke down the swap function into its own to separate the concerns
    - Testing with positive / negative / 0 / nil are done to cover all scenarios

--- 02_bubble/starfallz/main.go
+++ 02_bubble/starfallz/main.go
@@ -9,5 +9,29 @@ import (
 var out io.Writer = os.Stdout
 
 func main() {
-	fmt.Fprintln(out, "test")
+	fmt.Fprint(out, bubble([]int{3, 2, 1, 5}))
+}
+
+func bubble(s []int) []int {
+	for i := 0; i < len(s); i++ {
+		isSwapped := false
+		for j := 0; j < len(s)-1; j++ {
+			swap(s, j, &isSwapped)
+		}
+
+		if !isSwapped {
+			break
+		}
+	}
+
+	return s
+}
+
+func swap(s []int, j int, isSwapped *bool) {
+	tempVal := s[j+1]
+	if s[j] > tempVal {
+		s[j+1] = s[j]
+		s[j] = tempVal
+		*isSwapped = true
+	}
 }
--- 02_bubble/starfallz/main_test.go
+++ 02_bubble/starfallz/main_test.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"bytes"
+	"strconv"
+	"testing"
+)
+
+func TestMainFunction(t *testing.T) {
+	t.Run("Test main to return string values of bubble([]int{3, 2, 1, 5}) with proper formatting", func(t *testing.T) {
+		var buf bytes.Buffer
+		out = &buf
+
+		main()
+
+		expected := strconv.Quote("[1 2 3 5]")
+		actual := strconv.Quote(buf.String())
+
+		if expected != actual {
+			t.Errorf("Unexpected output, expected: %s, actual: %s", expected, actual)
+		}
+	})
+}
+
+func TestBubbleFunction(t *testing.T) {
+	testCases := []struct {
+		description string
+		input       []int
+		expected    []int
+	}{
+		{"Test bubble sorting from already sorted array", []int{1, 2, 3, 4}, []int{1, 2, 3, 4}},
+		{"Test bubble sorting with negative numbers", []int{-1, -2, -3, -4}, []int{-4, -3, -2, -1}},
+		{"Test bubble sorting with negative numbers", []int{1, -1, -2, 3, 2, -3, -4, 0, 4},
+			[]int{-4, -3, -2, -1, 0, 1, 2, 3, 4}},
+		{"Test bubble sorting sorting with Zero", []int{0}, []int{0}},
+		{"Test bubble sorting with nil", nil, nil},
+	}
+
+	for _, testCase := range testCases {
+		input := testCase.input
+		expected := testCase.expected
+
+		t.Run(testCase.description, func(t *testing.T) {
+			result := bubble(input)
+
+			for i, actual := range result {
+				if expected[i] != actual {
+					t.Errorf("Unexpected output, expected: %d, actual: %d", expected[i], actual)
+				}
+			}
+		})
+	}
+}
