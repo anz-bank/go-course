63be261a357bfa9ecb5e247883fa7bfe7c0efe60
Alfred Xiao <alfred.xiao@anz.com>
2019-06-20 11:54:07 +1000

    Implement numeronyms and tests

---
+++ 04_numeronym/alfredxiao/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var out io.Writer = os.Stdout
+
+func numeronyms(vals ...string) []string {
+	result := make([]string, len(vals))
+	for i, val := range vals {
+		result[i] = numeronym(val)
+	}
+	return result
+}
+
+func numeronym(val string) string {
+	length := len(val)
+	if length > 3 {
+		return fmt.Sprintf("%c%d%c", val[0], length-2, val[length-1])
+	}
+	return val
+}
+
+func main() {
+	fmt.Fprintln(out, numeronyms("abcd"))
+}
---
+++ 04_numeronym/alfredxiao/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSingleNumeronym(t *testing.T) {
+	assert.Equal(t, "", numeronym(""))
+	assert.Equal(t, "a", numeronym("a"))
+	assert.Equal(t, "ab", numeronym("ab"))
+	assert.Equal(t, "abc", numeronym("abc"))
+	assert.Equal(t, "a2d", numeronym("abcd"))
+	assert.Equal(t, "i18n", numeronym("internationalisation"))
+}
+
+func TestNumeronyms(t *testing.T) {
+	assert.Equal(t, []string{}, numeronyms())
+	assert.Equal(t, []string{"a"}, numeronyms("a"))
+	assert.Equal(t, []string{"a", "", "a2d"}, numeronyms("a", "", "abcd"))
+}
+
+func TestMain(t *testing.T) {
+	var buf bytes.Buffer
+	out = &buf
+	main()
+	assert.Equal(t, "[a2d]\n", buf.String())
+}
