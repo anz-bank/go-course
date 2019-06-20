343e917976e97b2287e5162455b95ce4668aacbf
Agung Haman <agung.haman@anz.com>
2019-06-20 16:46:30 +1000

    Added main that calls numeronym function to prints out numeronym representation of words
    - Had to use Fprint instead of println as per instruction as we want to test the output of the print outs
    - Added numeronyms func that takes one or more string as input and returns string slice of converted strings
    - Added convertString func which has all the core logic to convert a given string to numeronym representation
    - Had to use strconv.Iota() to convert int to string so that it can be joined by the first and last letter of the word input
    - Added tests to cover all cases including empty, non-alphanumerics, numerics, one or more string input

--- 04_numeronym/starfallz/main.go
+++ 04_numeronym/starfallz/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const minNumberOfLetters = 3
+
+var out io.Writer = os.Stdout
+
+func main() {
+	fmt.Fprint(out, numeronyms("accessibility", "Kubernetes", "abc"))
+}
+
+func numeronyms(vals ...string) []string {
+	result := make([]string, len(vals))
+	for i, val := range vals {
+		result[i] = convertString(val)
+	}
+	return result
+}
+
+func convertString(val string) string {
+	stringLength := len(val)
+	if stringLength > minNumberOfLetters {
+		return strings.Join([]string{val[:1], strconv.Itoa(stringLength - 2), val[stringLength-1 : stringLength]}, "")
+	}
+	return val
+}
--- 04_numeronym/starfallz/main_test.go
+++ 04_numeronym/starfallz/main_test.go
@@ -1,1 +1,82 @@
 package main
+
+import (
+	"bytes"
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+func TestMainFunction(t *testing.T) {
+	t.Run("Test main to return string values of numeronyms(accessibility, Kubernetes, abc) with proper formatting",
+		func(t *testing.T) {
+			var buf bytes.Buffer
+			out = &buf
+
+			main()
+
+			expected := strconv.Quote("[a11y K8s abc]")
+			actual := strconv.Quote(buf.String())
+
+			if expected != actual {
+				t.Errorf("Unexpected output, expected: %s, actual: %s", expected, actual)
+			}
+		})
+}
+
+func TestNumeronymsFunction(t *testing.T) {
+	testCases := []struct {
+		description string
+		input       []string
+		expected    []string
+	}{
+		{"Test numeronyms with one string input", []string{"accessibility"}, []string{"a11y"}},
+		{"Test numeronyms with multiple string input", []string{"accessibility", "Kubernetes", "abc"},
+			[]string{"a11y", "K8s", "abc"}},
+		{"Test numeronyms with nil to return empty slice", nil, []string{}},
+		{"Test numeronyms with empty to return empty slice", []string{}, []string{}},
+		{"Test numeronyms with empty string to return empty string", []string{""}, []string{""}},
+	}
+
+	for _, testCase := range testCases {
+		input := testCase.input
+		expected := testCase.expected
+
+		t.Run(testCase.description, func(t *testing.T) {
+			actual := numeronyms(input...)
+
+			if !reflect.DeepEqual(actual, expected) {
+				t.Errorf("Unexpected output, expected: %s, actual: %s", expected, actual)
+			}
+		})
+	}
+}
+
+func TestConvertStringFunction(t *testing.T) {
+	testCases := []struct {
+		description string
+		input       string
+		expected    string
+	}{
+		{"Test convertString with three letters not converted", "abc", "abc"},
+		{"Test convertString with two letters not converted", "ab", "ab"},
+		{"Test convertString with one letter not converted", "a", "a"},
+		{"Test convertString with four letters", "abcd", "a2d"},
+		{"Test convertString with numeric values", "1234567", "157"},
+		{"Test convertString with non-alphanumeric values", "*!+;'", "*3'"},
+		{"Test convertString with empty to return empty string", "", ""},
+	}
+
+	for _, testCase := range testCases {
+		input := testCase.input
+		expected := testCase.expected
+
+		t.Run(testCase.description, func(t *testing.T) {
+			actual := convertString(input)
+
+			if expected != actual {
+				t.Errorf("Unexpected output, expected: %s, actual: %s", expected, actual)
+			}
+		})
+	}
+}
