7c261e364dc7a05acb606a987ff4efc9648de45c
May <dmudushuhui@163.com>
2019-06-25 10:53:12 +1000

    updated testcases, added cases for String()

--- 05_stringer/marchitect/main_test.go
+++ 05_stringer/marchitect/main_test.go
@@ -20,3 +20,40 @@ func Test_main(t *testing.T) {
 		t.Errorf("Unexpected output in main(), expected = %v, actual = %v", expected, actual)
 	}
 }
+
+func Test_String(t *testing.T) {
+	testCases := map[string]struct {
+		input  IPAddr
+		output string
+	}{
+		"Empty": {
+			input:  IPAddr{},
+			output: "0.0.0.0",
+		},
+		"Partially Implemented": {
+			input:  IPAddr{1, 2},
+			output: "1.2.0.0",
+		},
+		"Example IP Address": {
+			input:  IPAddr{172, 16, 80, 100},
+			output: "172.16.80.100",
+		},
+		"Broadcast": {
+			input:  IPAddr{255, 255, 255, 255},
+			output: "255.255.255.255",
+		},
+		"Localhost": {
+			input:  IPAddr{127, 0, 0, 1},
+			output: "127.0.0.1",
+		},
+	}
+
+	for testCase, test := range testCases {
+		input, expected := test.input, test.output
+		actual := input.String()
+		if actual != expected {
+			t.Errorf("Unexpected output from test %s, expected = %s, actual = %s", testCase, expected, actual)
+		}
+	}
+
+}
