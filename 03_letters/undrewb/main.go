7b778f3191ee6bd9f2d6c1ce3840fca0f63e793a
Bucknell, Andrew <bucknela@corptst.anz.com>
2019-06-26 21:20:46 +1000

    Implement letters for lab 03 of go-course
    Shortening variable names
    Implements test of sortLetters
    
    Implement extensive test cases
    
    Add main_test to git tracking

---
+++ 03_letters/undrewb/main.go
@@ -0,0 +1,34 @@
+package main
+
+// https://github.com/anz-bank/go-course/tree/master/03_letters
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strings"
+)
+
+var out io.Writer = os.Stdout
+
+func main() {
+	fmt.Fprintln(out, strings.Join(sortLetters(letters("aba")), "\n"))
+}
+
+func sortLetters(m map[rune]int) []string {
+	strs := make([]string, 0, len(m))
+	for r, c := range m {
+		strs = append(strs, fmt.Sprintf("%c:%d", r, c))
+	}
+	sort.Strings(strs)
+	return strs
+}
+
+func letters(s string) map[rune]int {
+	rc := make(map[rune]int)
+	for _, r := range s {
+		rc[r]++
+	}
+	return rc
+}
