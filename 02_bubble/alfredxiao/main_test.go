0319910e71aa8f028b8131941384d73666265302
Alfred Xiao <xiaoyf@gmail.com>
2019-06-28 21:52:47 +1000

    Refactor test code, avoid repetitions

--- 02_bubble/alfredxiao/main_test.go
+++ 02_bubble/alfredxiao/main_test.go
@@ -7,26 +7,63 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBubbleEdgeCases(t *testing.T) {
-	assert.Equal(t, []int{}, bubble([]int{}))
-	assert.Equal(t, []int{1}, bubble([]int{1}))
-	assert.Equal(t, []int{1, 2}, bubble([]int{2, 1}))
-}
+func TestSorting(t *testing.T) {
+	cases := []struct {
+		name     string
+		input    []int
+		expected []int
+	}{
+		{
+			name:     "Edge Case - Empty slice",
+			input:    []int{},
+			expected: []int{},
+		},
+		{
+			name:     "Edge Case - Singular slice",
+			input:    []int{1},
+			expected: []int{1},
+		},
+		{
+			name:     "Edge Case - Smallest slice that requires sorting",
+			input:    []int{2, 1},
+			expected: []int{1, 2},
+		},
+		{
+			name:     "Best Case",
+			input:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		{
+			name:     "Average Case",
+			input:    []int{12, 2, 1, 6, 9, 3, 10},
+			expected: []int{1, 2, 3, 6, 9, 10, 12},
+		},
+		{
+			name:     "Worst Case",
+			input:    []int{9, 8, 7, 6, 5, 4, 3, 2, 1},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		{
+			name:     "With Duplicates",
+			input:    []int{3, 2, 3, 6, 8, 6, 9},
+			expected: []int{2, 3, 3, 6, 6, 8, 9},
+		},
+	}
 
-func TestBubbleBestCase(t *testing.T) {
-	assert.Equal(t, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}, bubble([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}))
-}
+	sortFns := map[string]func([]int) []int{
+		"Bubble Sort":    bubble,
+		"Insertion Sort": insertion,
+	}
 
-func TestBubbleAverageCase(t *testing.T) {
-	assert.Equal(t, []int{1, 2, 3, 6, 9, 10, 12}, bubble([]int{12, 2, 1, 6, 9, 3, 10}))
-}
-
-func TestBubbleWorstCase(t *testing.T) {
-	assert.Equal(t, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}, bubble([]int{9, 8, 7, 6, 5, 4, 3, 2, 1}))
-}
-
-func TestBubbleDuplicates(t *testing.T) {
-	assert.Equal(t, []int{2, 3, 3, 6, 6, 8, 9}, bubble([]int{3, 2, 3, 6, 8, 6, 9}))
+	for fname, fn := range sortFns {
+		fn := fn
+		for _, testCase := range cases {
+			testCase := testCase
+			t.Run(fname+":"+testCase.name, func(t *testing.T) {
+				assert.Equal(t, testCase.expected, fn(testCase.input))
+			})
+		}
+	}
 }
 
 func TestBubbleDoesNotTouchInput(t *testing.T) {
@@ -35,26 +72,10 @@ func TestBubbleDoesNotTouchInput(t *testing.T) {
 	assert.Equal(t, []int{3, 2, 1}, ns)
 }
 
-func TestInsertionEdgeCases(t *testing.T) {
-	assert.Equal(t, []int{}, insertion([]int{}))
-	assert.Equal(t, []int{1}, insertion([]int{1}))
-	assert.Equal(t, []int{1, 2}, insertion([]int{2, 1}))
-}
-
-func TestInsertionBestCase(t *testing.T) {
-	assert.Equal(t, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}, insertion([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}))
-}
-
-func TestInsertionAverageCase(t *testing.T) {
-	assert.Equal(t, []int{1, 2, 3, 6, 9, 10, 12}, insertion([]int{12, 2, 1, 6, 9, 3, 10}))
-}
-
-func TestInsertionWorstCase(t *testing.T) {
-	assert.Equal(t, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}, insertion([]int{9, 8, 7, 6, 5, 4, 3, 2, 1}))
-}
-
-func TestInsertionDuplicates(t *testing.T) {
-	assert.Equal(t, []int{2, 3, 3, 6, 6, 8, 9}, insertion([]int{3, 2, 3, 6, 8, 6, 9}))
+func TestInsertionDoesNotTouchInput(t *testing.T) {
+	ns := []int{3, 2, 1}
+	insertion(ns)
+	assert.Equal(t, []int{3, 2, 1}, ns)
 }
 
 func TestMain(t *testing.T) {
