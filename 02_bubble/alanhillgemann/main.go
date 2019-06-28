3b4b66bbe94562ff927366ddd22457f019f267d1
Alan Hillgemann <alan.hillgemann@anz.com>
2019-06-28 16:55:40 +1000

    Implement bubble sort solution and tests

---
+++ 02_bubble/alanhillgemann/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var out io.Writer = os.Stdout
+
+func main() {
+	fmt.Fprint(out, bubble([]int{3, 2, 1, 5}))
+}
+
+func bubble(arr []int) []int {
+	for i := 0; i <= len(arr)-1; i++ {
+		end := len(arr) - 2
+		for j := 0; j <= end; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}
