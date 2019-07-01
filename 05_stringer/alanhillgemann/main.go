61243f8251c830e5c25c2ea7559b5633b4ed297f
Alan Hillgemann <alan.hillgemann@anz.com>
2019-07-02 07:58:17 +1000

    Implement stringer solution and tests

---
+++ 05_stringer/alanhillgemann/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// IPAddr type accepts up to 4 bytes and prints string in IP Address format
+type IPAddr [4]byte
+
+var out io.Writer = os.Stdout
+
+func main() {
+	fmt.Fprint(out, IPAddr{127, 0, 0, 1})
+
+}
+
+func (ip IPAddr) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
+}
