5f9ea2f244d026fe2a2c617e02b85de3836b5e2e
Alan Hillgemann <alan.hillgemann@anz.com>
2019-07-11 07:48:04 +1000

    Change variable delaration shorthand, for statement range, descriptive variable names, optimise letters func

--- 03_letters/alanhillgemann/main.go
+++ 03_letters/alanhillgemann/main.go
@@ -15,25 +15,18 @@ func main() {
 }
 
 func letters(s string) map[rune]int {
-	runeMap := make(map[rune]int)
-	runeSlice := []rune(s)
-	for i := 0; i <= len(runeSlice)-1; i++ {
-		runeMap[runeSlice[i]]++
+	letterTotalMap := map[rune]int{}
+	for _, r := range s {
+		letterTotalMap[r]++
 	}
-	return runeMap
+	return letterTotalMap
 }
 
 func sortLetters(m map[rune]int) []string {
-	stringSlice := make([]string, 0)
-	mapKeys := make([]string, 0)
-	mapValues := make([]int, 0)
+	letterTotalStrings := []string{}
 	for k, v := range m {
-		mapKeys = append(mapKeys, string(k))
-		mapValues = append(mapValues, v)
+		letterTotalStrings = append(letterTotalStrings, fmt.Sprintf("%s:%d", string(k), v))
 	}
-	for i := 0; i <= len(m)-1; i++ {
-		stringSlice = append(stringSlice, fmt.Sprintf("%s:%d", mapKeys[i], mapValues[i]))
-	}
-	sort.Strings(stringSlice)
-	return stringSlice
+	sort.Strings(letterTotalStrings)
+	return letterTotalStrings
 }
