5f18865f07fe6faa6152468a3aab0800b2577617
Alan Hillgemann <alan.hillgemann@anz.com>
2019-07-19 17:18:16 +1000

    Move method close to type

--- 05_stringer/alanhillgemann/main.go
+++ 05_stringer/alanhillgemann/main.go
@@ -9,13 +9,12 @@ import (
 // IPAddr type accepts up to 4 bytes and prints string in IP Address format
 type IPAddr [4]byte
 
+func (ip IPAddr) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
+}
+
 var out io.Writer = os.Stdout
 
 func main() {
 	fmt.Fprint(out, IPAddr{127, 0, 0, 1})
-
-}
-
-func (ip IPAddr) String() string {
-	return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
 }
