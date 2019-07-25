7e9e24aebee96ac74c7dcecca910490eea8ef0bb
Alan Hillgemann <alan.hillgemann@anz.com>
2019-07-25 17:38:07 +1000

    Remove int slice and remove verbosity, change 1st result from 0 to 1, move logging to main

--- 01_fib/alanhillgemann/main.go
+++ 01_fib/alanhillgemann/main.go
@@ -10,33 +10,21 @@ import (
 var out io.Writer = os.Stdout
 
 func main() {
+	log.SetFlags(log.Ldate)
 	fib(7)
 }
 
 func fib(n int) {
 	if n < 0 {
-		log.SetFlags(0)
 		log.Printf("Fib of %d is not available. Defaulting to minimum fib of 0", n)
 		n = 0
 	} else if n > 45 {
-		log.SetFlags(0)
 		log.Printf("Fib of %d is not available. Defaulting to maximum fib of 45", n)
 		n = 45
 	}
-	fibSlice := []int{}
-	nextFib := 0
-	for i := 0; i <= n; i++ {
-		switch len(fibSlice) {
-		case 0:
-			fibSlice = append(fibSlice, 0)
-		case 1:
-			fibSlice = append(fibSlice, 1)
-		default:
-			nextFib = fibSlice[i-1] + fibSlice[i-2]
-			fibSlice = append(fibSlice, nextFib)
-		}
-	}
-	for _, v := range fibSlice {
-		fmt.Fprintln(out, v)
+	n1, n2 := 1, 1
+	for i := 0; i < n; i++ {
+		fmt.Fprintln(out, n1)
+		n1, n2 = n2, n1+n2
 	}
 }
