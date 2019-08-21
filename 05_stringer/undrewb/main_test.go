142992e2177386971ac97fdce0ad50f6dac62321
Bucknell, Andrew <bucknela@corptst.anz.com>
2019-08-21 20:03:18 +1000

    Fix formatting issues with test cases raised by reviewers
    Change assert parameter order to be expected then actual
    Fix formatting of test case structs

--- 05_stringer/undrewb/main_test.go
+++ 05_stringer/undrewb/main_test.go
@@ -18,7 +18,7 @@ func TestMainOutput(t *testing.T) {
 	want := strconv.Quote("127.0.0.1\n")
 	got := strconv.Quote(buf.String())
 
-	assert.Equal(t, got, want)
+	assert.Equal(t, want, got)
 }
 
 var stringerData = []struct {
@@ -26,24 +26,35 @@ var stringerData = []struct {
 	input IPAddr
 	want  string
 }{
-	{name: "localhost",
+	{
+		name:  "localhost",
 		input: IPAddr{127, 0, 0, 1},
-		want:  "127.0.0.1"},
-	{name: "four octet addr",
+		want:  "127.0.0.1",
+	},
+	{
+		name:  "four octet addr",
 		input: IPAddr{68, 2, 44, 125},
-		want:  "68.2.44.125"},
+		want:  "68.2.44.125",
+	},
 	{name: "three octet addr",
 		input: IPAddr{68, 2, 125},
-		want:  "68.2.125.0"},
-	{name: "two octet addr",
+		want:  "68.2.125.0",
+	},
+	{
+		name:  "two octet addr",
 		input: IPAddr{9, 5},
-		want:  "9.5.0.0"},
-	{name: "one octet addr",
+		want:  "9.5.0.0",
+	},
+	{
+		name:  "one octet addr",
 		input: IPAddr{144},
-		want:  "144.0.0.0"},
-	{name: "empty",
+		want:  "144.0.0.0",
+	},
+	{
+		name:  "empty",
 		input: IPAddr{},
-		want:  "0.0.0.0"},
+		want:  "0.0.0.0",
+	},
 }
 
 func TestStringer(t *testing.T) {
@@ -51,7 +62,7 @@ func TestStringer(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			got := fmt.Sprint(tt.input)
-			assert.Equal(t, got, tt.want)
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
