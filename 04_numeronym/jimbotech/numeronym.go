c2850ed9af602553cf19c77ceac1961cddf58c14
Jim hejtmanek <jim.hejtmanek@mlcinsurance.com.au>
2019-09-19 16:42:10 +1000

    Changes resulting from PR review

--- 04_numeronym/jimbotech/numeronym.go
+++ 04_numeronym/jimbotech/numeronym.go
@@ -5,68 +5,47 @@ import (
 	"io"
 	"os"
 	"strings"
-	"unicode"
 )
 
 var out io.Writer = os.Stdout
 
-// following rules from https://www.revolvy.com/page/Numeronym
 // Program need not convert words with length 3, and should not convert shorter words.
 // If an l (lowercase ell) would occur before a 1 (one), it should be made uppercase.
 // If an I (uppercase eye) would occur before a 1 (one), it should be made lowercase.
-// Input will be printable ASCII with spaces.
-func wordMixer() func(inputStr string) string {
-
+func wordMixer(rawInput string) string {
 	knownConversions := map[string]string{
 		"year 2038":        "Y2K38",
 		"year 2000":        "Y2K",
 		"i love you":       "143",
 		"eyjafjallajökull": "E15",
 	}
-
-	return func(inputStr string) string {
-
-		inputStr = strings.TrimSpace(inputStr)
-		runes := []rune(inputStr)
-
-		if len(runes) <= 3 {
-			return inputStr
-		}
-
-		lowerRunes := []rune(inputStr)
-		for i, v := range lowerRunes {
-			lowerRunes[i] = unicode.ToLower(v)
-		}
-		if value, found := knownConversions[string(lowerRunes)]; found {
-			return value
-		}
-
-		firstChar := runes[0]
-		lastChar := runes[len(runes)-1]
-		len := len(runes) - 2
-
-		if len > 9 && len < 20 {
-			if firstChar == 'I' {
-				firstChar = 'i'
-			} else if firstChar == 'l' {
-				firstChar = 'L'
-			}
-		}
-		return fmt.Sprintf("%c%d%c", firstChar, len, lastChar)
+	rawInput = strings.TrimSpace(rawInput)
+	rw := []rune(rawInput)
+	if len(rw) <= 3 {
+		return rawInput
 	}
+	if known, found := knownConversions[strings.ToLower(rawInput)]; found {
+		return known
+	}
+	fc := rw[0]
+	lc := rw[len(rw)-1]
+	l := len(rw) - 2
+	if l >= 10 && l <= 19 {
+		if fc == 'I' {
+			fc = 'i'
+		} else if fc == 'l' {
+			fc = 'L'
+		}
+	}
+	return fmt.Sprintf("%c%d%c", fc, l, lc)
 }
-
 func numeronyms(vals ...string) []string {
-
-	f := wordMixer()
 	results := []string{}
-
 	for _, val := range vals {
-		results = append(results, f(val))
+		results = append(results, wordMixer(val))
 	}
 	return results
 }
-
 func main() {
 	fmt.Fprintln(out, numeronyms("accessibility", "Kubernetes", "abc"))
 }
--- 04_numeronym/jimbotech/numeronym_test.go
+++ 04_numeronym/jimbotech/numeronym_test.go
@@ -6,44 +6,36 @@ import (
 	"testing"
 )
 
-type numeronymTest struct {
-	input    string
-	expected string
-}
-
-var numTests = []numeronymTest{
-	{"Kubernetes", "K8s"},
-	{"I love You ", "143"},
-	{"localizability ", "L12y"},
-	{"   Interoperability ", "i14y"},
-	{"", ""},
-	{"year 2038", "Y2K38"},
-	{"somebody said hello", "s17o"},
-}
-
-type multiTest struct {
-	input    []string
-	expected []string
-}
-
-var multiNumTests = []multiTest{
-	{[]string{"Kubernetes", "localizability", "Interoperability", "I love You ", "abc"},
-		[]string{"K8s", "L12y", "i14y", "143", "abc"}},
-}
-
 func TestWordMixer(t *testing.T) {
-
-	f := wordMixer()
+	type numeronymTest struct {
+		input    string
+		expected string
+	}
+	var numTests = []numeronymTest{
+		{"Kubernetes", "K8s"},
+		{"I love You ", "143"},
+		{"localizability ", "L12y"},
+		{"   Interoperability ", "i14y"},
+		{"", ""},
+		{"year 2038", "Y2K38"},
+		{"somebody said hello", "s17o"},
+	}
 	for _, tt := range numTests {
-		actual := f(tt.input)
+		actual := wordMixer(tt.input)
 		if actual != tt.expected {
 			t.Errorf("wordMixer(%v): expected %v, actual %v", tt.input, tt.expected, actual)
 		}
 	}
 }
-
 func TestNumeronyms(t *testing.T) {
-
+	type multiTest struct {
+		input    []string
+		expected []string
+	}
+	var multiNumTests = []multiTest{
+		{[]string{"Kubernetes", "localizability", "Interoperability", "I love You ", "abc"},
+			[]string{"K8s", "L12y", "i14y", "143", "abc"}},
+	}
 	for _, tt := range multiNumTests {
 		actual := numeronyms(tt.input...)
 		if !reflect.DeepEqual(actual, tt.expected) {
@@ -51,14 +43,12 @@ func TestNumeronyms(t *testing.T) {
 		}
 	}
 }
-
 func TestMain(t *testing.T) {
 	expected := "[a11y K8s abc]\n"
 	var buf bytes.Buffer
 	out = &buf
 	main()
 	actual := buf.String()
-
 	if actual != expected {
 		t.Errorf("main expected %v, got %v", expected, actual)
 	}
