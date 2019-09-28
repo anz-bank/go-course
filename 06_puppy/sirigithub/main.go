dd379c05219dbca1ff98025530d553a8f188f49d
Sirisha Vadrevu <sirideals@gmail.com>
2019-09-29 08:13:07 +1000

    Remove debug messages

--- 06_puppy/sirigithub/main.go
+++ 06_puppy/sirigithub/main.go
@@ -12,11 +12,8 @@ func main() {
 	mapStore := NewMapStore()
 	puppy1 := Puppy{Breed: "Mix", Color: "White", Value: "Free"}
 	id := mapStore.CreatePuppy(&puppy1)
-	println(id)
 	syncStore := NewSyncStore()
 	id2 := syncStore.CreatePuppy(&puppy1)
-	println(id2)
-
 	p1, _ := mapStore.ReadPuppy(id)
 	fmt.Fprintln(out, "Read puppy from Mapstore with ID:", p1.ID)
 	p2, _ := syncStore.ReadPuppy(id2)
