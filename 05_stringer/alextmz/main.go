1e3331eac906c8d2fd61d7f1939cfa2a4839b902
alexmedeiros <alexmedeiros@gmail.com>
2019-10-06 17:49:40 +1100

    Address review issues

--- 05_stringer/alextmz/main.go
+++ 05_stringer/alextmz/main.go
@@ -6,15 +6,14 @@ import (
 	"os"
 )
 
-// IPAddr is below, violets are blue and this is here to shut up golint :-)
-type IPAddr [4]byte
+type ipAddr [4]byte
 
 var out io.Writer = os.Stdout
 
 func main() {
-	fmt.Fprintln(out, IPAddr{127, 0, 0, 1})
+	fmt.Fprintln(out, ipAddr{127, 0, 0, 1})
 }
 
-func (i IPAddr) String() string {
+func (i ipAddr) String() string {
 	return fmt.Sprintf("%d.%d.%d.%d", i[0], i[1], i[2], i[3])
 }
--- 05_stringer/alextmz/main_test.go
+++ 05_stringer/alextmz/main_test.go
@@ -7,30 +7,35 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_IPAddrString(t *testing.T) {
+func TestIPAddrString(t *testing.T) {
 	var tests = map[string]struct {
-		arg  IPAddr
+		arg  ipAddr
 		want string
 	}{
 		"empty IPv4": {
-			IPAddr{},
-			"0.0.0.0"},
+			ipAddr{},
+			"0.0.0.0",
+		},
 		"8bits IPv4": {
-			IPAddr{254},
-			"254.0.0.0"},
+			ipAddr{254},
+			"254.0.0.0",
+		},
 		"16bits IPv4": {
-			IPAddr{254, 254},
-			"254.254.0.0"},
+			ipAddr{254, 254},
+			"254.254.0.0",
+		},
 		"24bits IPv4": {
-			IPAddr{254, 254, 254},
-			"254.254.254.0"},
+			ipAddr{254, 254, 254},
+			"254.254.254.0",
+		},
 		"full IPv4": {
-			IPAddr{254, 254, 254, 254},
-			"254.254.254.254"},
+			ipAddr{254, 254, 254, 254},
+			"254.254.254.254",
+		},
 	}
 
-	for name, tt := range tests {
-		test := tt
+	for name, test := range tests {
+		test := test
 		t.Run(name, func(t *testing.T) {
 			actual := test.arg.String()
 			assert.Equal(t, test.want, actual)
@@ -38,14 +43,11 @@ func Test_IPAddrString(t *testing.T) {
 	}
 }
 
-func Test_main(t *testing.T) {
+func TestMain(t *testing.T) {
 	want := "127.0.0.1\n"
-
-	t.Run("main test", func(t *testing.T) {
-		var buf bytes.Buffer
-		out = &buf
-		main()
-		got := buf.String()
-		assert.Equal(t, got, want)
-	})
+	var buf bytes.Buffer
+	out = &buf
+	main()
+	got := buf.String()
+	assert.Equal(t, want, got)
 }
